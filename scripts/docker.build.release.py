ad004a7112a0fdd936ae4af63e8a323377e8916d
Radu B <radu.buciuceanu@gmail.com>
2021-05-26 13:10:43 +0300

    [chaos] Create release build script

---
+++ scripts/docker.build.release.py
@@ -0,0 +1,12 @@
+from os import system
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+
+parser.add_argument('--tag',
+                    default='nerdscomputing/hermes.web:latest',
+                    help='The tag for the new created docker image.')
+
+args = parser.parse_args()
+
+system(f'docker build -t {args.tag} -f dockerfile.release .')
