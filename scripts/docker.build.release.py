b758caf798df1b41f51e1c9b67f139f5dd729053
Radu B <radu.buciuceanu@gmail.com>
2021-06-01 14:46:59 +0300

    [chaos] Change release script

--- scripts/docker.build.release.py
+++ scripts/docker.build.release.py
@@ -7,6 +7,10 @@ parser.add_argument('--tag',
                     default='nerdscomputing/hermes.web:latest',
                     help='The tag for the new created docker image.')
 
+parser.add_argument('--version',
+                    default='2021.0.0',
+                    help='The semantic version of the web.')
+
 args = parser.parse_args()
 
-system(f'docker build -t {args.tag} -f dockerfile.release .')
+system(f'docker build -t {args.tag} -f dockerfile.release --build-arg VERSION={args.version} .')
