ae20ee710f48973f321b345f9edd6effbd59ccf0
Codrut <constantinescu321@gmail.com>
2021-06-30 12:23:51 +0300

    [HER-32]Add status/version path and component

--- src/startup/routes.jsx
+++ src/startup/routes.jsx
@@ -1,6 +1,11 @@
 import { Home } from 'home'
+import { Version } from 'version'
 
 export const routes = [{
+  path: '/status/version',
+  Component: Version,
+},
+{
   path: '/',
   Component: Home,
 }]
