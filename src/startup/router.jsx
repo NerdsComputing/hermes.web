d617b071f0afa24ea42ae79974afb903c052bcdb
casian <casian.platon@ulbsibiu.ro>
2021-06-30 11:41:03 +0300

    [HER-35]Remove redundant parenthesis

--- src/startup/router.jsx
+++ src/startup/router.jsx
@@ -3,10 +3,9 @@ import React from 'react'
 import { BrowserRouter, Switch } from 'react-router-dom'
 import { Mapping } from 'startup/mapping'
 
-export const Router = () => (
-  <BrowserRouter>
+export const Router = () => <BrowserRouter>
     <Switch>
       <Mapping />
     </Switch>
   </BrowserRouter>
-)
+
