13a589be1224e4841490585828e5d28fcf27e040
casian <casian.platon@ulbsibiu.ro>
2021-06-30 11:06:28 +0300

    [HER-35]Follow-up-Change path to route.path

--- src/startup/mapping.jsx
+++ src/startup/mapping.jsx
@@ -5,7 +5,7 @@ import { Route } from 'react-router-dom'
 
 export const Mapping = () => <>
   {
-    routes.map((route, path) => <Route path={route.path} key={path}>
+    routes.map(route => <Route path={route.path} key={route.path}>
       <route.Component />
     </Route>)
   }
