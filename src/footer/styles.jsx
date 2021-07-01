9be2dff162e52118debfdc8dfb5aae191b5586a9
casian <casian.platon@ulbsibiu.ro>
2021-07-01 12:43:00 +0300

    [HER-39]Resolve Brave issue

--- src/footer/styles.jsx
+++ src/footer/styles.jsx
@@ -2,7 +2,9 @@ import { makeStyles } from '@material-ui/core'
 
 export const useStyles = makeStyles({
   paragraph: { color: 'white' },
-  footer: { backgroundColor: 'blue', height: '11.5vh' },
+  footer: { backgroundColor: 'blue', height: '10%' },
   gridHeight: { height: '100%' },
-  divHeight: { height: '80vh' },
+  divHeight: { height: '90%' },
+  divPageHeight: { height: '100vh' },
+  h1: { margin: 0 },
 })
--- src/home/index.jsx
+++ src/home/index.jsx
@@ -3,8 +3,7 @@ import { Footer } from 'footer/footer'
 
 import { useStyles } from 'footer/styles'
 
-export const Home = () => <div>
-  <h1>Hermes Web</h1>
-  <div className={useStyles().divHeight} />
+export const Home = () => <div className={useStyles().divPageHeight}>
+  <div className={useStyles().divHeight}><h1 className={useStyles().h1}>Hermes Web</h1></div>
   <Footer />
 </div>
