c50408eb74b0ef7571cdbdc81b61be1e0898367a
casian <casian.platon@ulbsibiu.ro>
2021-07-02 00:24:42 +0300

    [HER-39]Follow-up-Remove xs, sm from footergrid

--- src/footer/footergrid.jsx
+++ src/footer/footergrid.jsx
@@ -3,7 +3,7 @@ import { Grid } from '@material-ui/core'
 
 import { useStyles } from 'footer/styles'
 
-export const Footergrid = () => <Grid container alignContent={'center'} justify={'center'} item xs={12} sm={4}>
+export const Footergrid = () => <Grid container alignContent={'center'} justify={'center'}>
   <p className={useStyles().paragraph}>
     Made with love by
     <a href={'about.jsx'}> Hermes team</a>
