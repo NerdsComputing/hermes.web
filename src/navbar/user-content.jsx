6a94f2b58eb7386b1194a836c8b3bf8bda0bbd83
Codrut <constantinescu321@gmail.com>
2021-07-01 13:05:58 +0300

    [HER-38]Follow-up Add sm md and xl breakpoints

--- src/navbar/user-content.jsx
+++ src/navbar/user-content.jsx
@@ -5,7 +5,7 @@ import { Gravatar } from 'navbar/gravatar'
 import { Email } from 'navbar/email'
 import { useStyles } from 'navbar/style'
 
-export const UserContent = () => <Grid item xs={4} lg={6}>
+export const UserContent = () => <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
   <Grid container direction={'row'} alignItems={'center'} justify={'flex-end'} className={useStyles().userContent}>
     <Email />
     <Gravatar />
