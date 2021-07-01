0c6251a7484ee06bae4c21a72dc7403f4df92c96
casian <casian.platon@ulbsibiu.ro>
2021-07-01 11:23:20 +0300

    [HER-39]Add footer to index

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,3 +1,10 @@
 import React from 'react'
+import { Footer } from 'footer/footer'
 
-export const Home = () => <h1>Hermes Web</h1>
+import { useStyles } from 'footer/styles'
+
+export const Home = () => <div>
+  <h1>Hermes Web</h1>
+  <div className={useStyles().divHeight} />
+  <Footer />
+</div>
