a931e999b0b3f58132b988c75ca8510f3532bac6
casian <casian.platon@ulbsibiu.ro>
2021-07-02 01:55:15 +0300

    [HER-39]Follow-up-H1 on new line

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -4,6 +4,8 @@ import { Footer } from 'footer/footer'
 import { useStyles } from 'footer/styles'
 
 export const Home = () => <div className={useStyles().divPageHeight}>
-  <div className={useStyles().divHeight}><h1 className={useStyles().h1}>Hermes Web</h1></div>
+  <div className={useStyles().divHeight}>
+    <h1 className={useStyles().h1}>Hermes Web</h1>
+  </div>
   <Footer />
 </div>
