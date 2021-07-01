38a49ca49ee390ee7125c545667fb5ca556f418c
Codrut <constantinescu321@gmail.com>
2021-07-01 12:21:37 +0300

    [HER-38]Add grid item

--- src/navbar/title.jsx
+++ src/navbar/title.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
+import { Grid, Typography } from '@material-ui/core'
 
-import { Typography } from '@material-ui/core'
 import { useStyles } from 'navbar/style'
 
-export const Title = () => {
-  const classes = useStyles()
-
-  return <Typography variant={'h6'} className={classes.title}>
+export const Title = () => <Grid item xs={8} lg={6}>
+  <Typography variant={'h6'} className={useStyles().title}>
     Hermes
   </Typography>
-}
+</Grid>
