3b715e0d7096cf1c9f25f65d1aaa6b17790532de
Codrut <constantinescu321@gmail.com>
2021-07-01 17:38:54 +0300

    [HER-38]Follow-up Add margin class

--- src/navbar/style.js
+++ src/navbar/style.js
@@ -3,8 +3,6 @@ import { makeStyles } from '@material-ui/core'
 export const useStyles = makeStyles({
   root: { minHeight: '5vh' },
   title: { margin: '0 0 0 15px' },
-  userContent: {
-    height: '100%',
-    paddingRight: 15,
-  },
+  userContent: { height: '100%' },
+  margin: { margin: 5 },
 })
