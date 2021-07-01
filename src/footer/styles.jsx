0bdf51feeffdf360bcb46bf8b183b04ba56bee4c
casian <casian.platon@ulbsibiu.ro>
2021-07-01 11:24:11 +0300

    [HER-39]Create style file

---
+++ src/footer/styles.jsx
@@ -0,0 +1,8 @@
+import { makeStyles } from '@material-ui/core'
+
+export const useStyles = makeStyles({
+  paragraph: { color: 'white' },
+  footer: { backgroundColor: 'blue', height: '11.5vh' },
+  gridHeight: { height: '100%' },
+  divHeight: { height: '80vh' },
+})
