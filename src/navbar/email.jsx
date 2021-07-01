6ea10510637379aa60488947aafc8beebb96e1a3
Codrut <constantinescu321@gmail.com>
2021-07-01 12:22:41 +0300

    [HER-38]Delete style

--- src/navbar/email.jsx
+++ src/navbar/email.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
-
 import { Typography } from '@material-ui/core'
-import { useStyles } from 'navbar/style'
 
-export const Email = () => {
-  const classes = useStyles()
-
-  return <Typography className={classes.email}>
-    Welcome, email
-  </Typography>
-}
+export const Email = () => <Typography>
+  Welcome, email
+</Typography>
