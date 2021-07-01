0e36117dcd627b70d836f8b40965dffce84122ee
Codrut <constantinescu321@gmail.com>
2021-07-01 12:22:27 +0300

    [HER-38]Delete style

--- src/navbar/gravatar.jsx
+++ src/navbar/gravatar.jsx
@@ -1,10 +1,4 @@
 import React from 'react'
 import CodeIcon from '@material-ui/icons/Code'
 
-import { useStyles } from 'navbar/style'
-
-export const Gravatar = () => {
-  const classes = useStyles()
-
-  return <CodeIcon className={classes.gravatar} />
-}
+export const Gravatar = () => <CodeIcon />
