6badb6f2b1431ced70f0ebf491d4806f3a05f8fb
Codrut <constantinescu321@gmail.com>
2021-07-02 15:37:46 +0300

    [HER-38]Follow-up Create container for content

---
+++ src/core/navbar/content/container.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+import PropTypes from 'prop-types'
+
+import { useStyles } from 'core/navbar/content/style'
+
+export const Container = ({ children }) => <Grid item xs={5} sm={5} md={5} lg={5} xl={5}
+                                                 className={useStyles().marginRight}>
+  <Grid container alignItems={'center'} justify={'flex-end'}>
+    {children}
+  </Grid>
+</Grid>
+
+Container.propTypes = { children: PropTypes.element.isRequired }
