5d587707a4ba591f8669f96c0069548fb6edda4a
Codrut <constantinescu321@gmail.com>
2021-07-02 15:31:44 +0300

    [HER-38]Follow-up Delete file

--- src/navbar/email.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Typography } from '@material-ui/core'
-
-import { useStyles } from 'navbar/style'
-
-export const Email = () => <Typography className={useStyles().margin}>
-  Welcome, email
-</Typography>
--- src/navbar/gravatar.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import CodeIcon from '@material-ui/icons/Code'
-
-import { useStyles } from 'navbar/style'
-
-export const Gravatar = () => <CodeIcon className={useStyles().margin} />
