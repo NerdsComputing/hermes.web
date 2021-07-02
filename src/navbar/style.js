59608acd150490abb9ef8f244dacc14b8467c28f
Codrut <constantinescu321@gmail.com>
2021-07-02 15:32:17 +0300

    [HER-38]Follow-up Delete file

--- src/navbar/style.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from '@material-ui/core'
-
-export const useStyles = makeStyles({
-  root: { minHeight: '5vh' },
-  title: { margin: '0 0 0 15px' },
-  userContent: { height: '100%' },
-  margin: { margin: 5 },
-})
