3f44ea836bc1b0cf7511daedd9769c689af784be
Codrut <constantinescu321@gmail.com>
2021-07-02 15:35:13 +0300

    [HER-38]Follow-up Create title component

---
+++ src/core/navbar/title/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Grid, Typography } from '@material-ui/core'
+
+import { useStyles } from 'core/navbar/title/style'
+
+export const Title = () => <Grid item xs={5} sm={5} md={5} lg={5} xl={5} className={useStyles().root}>
+  <Typography variant={'h6'}>
+    Hermes
+  </Typography>
+</Grid>
