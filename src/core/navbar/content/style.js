0f9898602bc0c48a97a4e908eb9a54e8596f33a8
Codrut <constantinescu321@gmail.com>
2021-07-02 15:35:43 +0300

    [HER-38]Follow-up Create style for appbar

---
+++ src/core/navbar/content/style.js
@@ -0,0 +1,7 @@
+import { makeStyles } from '@material-ui/core'
+
+export const useStyles = makeStyles(theme => ({
+  root: { height: '100%' },
+  marginRight: { marginRight: 5 },
+  contentMargin: { margin: theme.spacing(1) },
+}))
