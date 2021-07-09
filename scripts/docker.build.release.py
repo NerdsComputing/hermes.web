fa8dacb1c75d4f233c0c66a9204c3e7f6af2d7ff
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2021-07-09 15:35:05 +0300

    [HER-61] Configure informational version (#13)
    * [HER-61] Change release dockerfile
    
    * [HER-61] Create version file
    
    * [HER-61] Change build script
    
    * [HER-61] Fix release script

--- scripts/docker.build.release.py
+++ scripts/docker.build.release.py
@@ -7,10 +7,14 @@ parser.add_argument('--tag',
                     default='nerdscomputing/hermes.web:latest',
                     help='The tag for the new created docker image.')
 
-parser.add_argument('--version',
+parser.add_argument('--shortVersion',
                     default='2021.0.0',
-                    help='The semantic version of the web.')
+                    help='The short semantic version of the web.')
+
+parser.add_argument('--longVersion',
+                    default='2021.0.0.Branch.Sha',
+                    help='The long semantic version of the web.')
 
 args = parser.parse_args()
 
-system(f'docker build -t {args.tag} -f dockerfile.release --build-arg VERSION={args.version} .')
+system(f'docker build -t {args.tag} -f dockerfile.release --build-arg SHORT_VERSION={args.shortVersion} --build-arg LONG_VERSION={args.longVersion} .')
