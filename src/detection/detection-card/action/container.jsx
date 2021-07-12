079e72399ae40f7094022cb42c409ce4895ad9c1
Codrut <constantinescu321@gmail.com>
2021-07-12 14:41:35 +0300

    [HER-17]Create wrapper for card action

---
+++ src/detection/detection-card/action/container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CardActions as MaterialCardActions, Grid } from '@material-ui/core'
+import PropTypes from 'prop-types'
+
+import { useStyles } from 'detection/detection-card/action/style'
+
+export const CardAction = ({ children }) => <MaterialCardActions className={useStyles().buttonsContainer}>
+  <Grid container justify={'space-around'} alignContent={'center'}>
+    {children}
+  </Grid>
+</MaterialCardActions>
+
+CardAction.propTypes = { children: PropTypes.array.isRequired }
