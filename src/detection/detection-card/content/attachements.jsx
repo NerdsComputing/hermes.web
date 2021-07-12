a0c780a4b0f52f4f8430cc3bac616cd4affc05ee
Codrut <constantinescu321@gmail.com>
2021-07-12 14:39:04 +0300

    [HER-17]Create attachements component

---
+++ src/detection/detection-card/content/attachements.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box, Typography } from '@material-ui/core'
+import DescriptionIcon from '@material-ui/icons/Description'
+
+import { useStyles } from 'detection/detection-card/content/style'
+
+export const Attachements = () => <Box className={useStyles().contentItem}>
+  <DescriptionIcon />
+  <Typography variant={'h6'} align={'center'} className={useStyles().contentText}>
+    Attachements
+  </Typography>
+</Box>
