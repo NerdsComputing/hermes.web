c16ad6233db67040556c94baabe6d0ebbc304454
Codrut <constantinescu321@gmail.com>
2021-07-12 14:32:43 +0300

    [HER-17]Create detection page

---
+++ src/detection/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import { DetectionWrapper } from 'detection/container'
+import { DetectionCard } from 'detection/detection-card'
+
+export const DetectionPage = () => <DetectionWrapper>
+  <DetectionCard />
+</DetectionWrapper>
