9b536981a36f93d1ccfe85b3d3d5dba48eefba8e
bianca <bia.topircean@gmail.com>
2021-07-12 15:55:58 +0300

    [HER-13] Add provider

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 
 import { Router } from 'startup/router'
+import { createStore } from 'startup/create-store'
 
-export const App = () => <Router />
+export const App = () => <Provider store={createStore()}>
+  <Router />
+</Provider>
--- src/startup/create-store.js
+++ src/startup/create-store.js
@@ -1,5 +1,5 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 
 import { rootReducer } from 'startup/root-reducer'
 
-export const createStore = () => configureStore({reducer: rootReducer})
+export const createStore = () => configureStore({ reducer: rootReducer })
