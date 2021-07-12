52cd352843b2552383ba9940fe3381e56bde11f2
Codrut <constantinescu321@gmail.com>
2021-07-12 14:32:12 +0300

    [HER-17]Create style for detection page

---
+++ src/detection/style.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  width: '100vw',
+  height: '100vh',
+  backgroundColor: '#d4fae3',
+}
+
+export const useStyles = makeStyles({ root })
