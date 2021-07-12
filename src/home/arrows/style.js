cd3462f5f94b8d85a60050028d5681ad5eb5ad58
andreea-dumitrascu <72592844+andreea-dumitrascu@users.noreply.github.com>
2021-07-12 15:12:16 +0300

    [HER-64] Remove horizontal scroll on home page (#14)

--- src/home/arrows/style.js
+++ src/home/arrows/style.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core'
 export const useStyles = makeStyles(() => ({
   arrow: {
     paddingTop: '22%',
-    paddingLeft: '10%',
-    paddingRight: '10%',
+    paddingLeft: '1%',
+    paddingRight: '1%',
   },
 }))
