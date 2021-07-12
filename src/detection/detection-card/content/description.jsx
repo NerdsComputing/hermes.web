54ea67f0c4fd7e5e9afa029aecabfa3259059b14
Codrut <constantinescu321@gmail.com>
2021-07-12 14:39:16 +0300

    [HER-17]Create description component

---
+++ src/detection/detection-card/content/description.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box, Typography } from '@material-ui/core'
+import DescriptionIcon from '@material-ui/icons/Description'
+
+import { useStyles } from 'detection/detection-card/content/style'
+
+export const Description = () => <Box className={useStyles().contentItem}>
+  <DescriptionIcon />
+  <Typography variant={'h6'} className={useStyles().contentText}>
+    Description
+  </Typography>
+</Box>
