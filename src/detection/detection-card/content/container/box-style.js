1c7ef73789f0c37f6bf0da13a69505111d993e8a
Codrut <constantinescu321@gmail.com>
2021-07-13 22:51:07 +0300

    [HER-17]Follow-up Create box-style

---
+++ src/detection/detection-card/content/container/box-style.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  width: '60%',
+  height: '60%',
+  padding: 15,
+  border: '5px solid black',
+}
+
+export const useStyles = makeStyles({ root })
