d4eacf53eb29459d5739fd41b3723122bffcac4c
Codrut <constantinescu321@gmail.com>
2021-07-13 13:00:03 +0300

    [HER-17]Follow-up Change components path

--- src/detection/detection-card/content/index.jsx
+++ src/detection/detection-card/content/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import { CreationAt } from 'detection/detection-card/content/creation-at'
-import { Description } from 'detection/detection-card/content/description'
-import { Attachements } from 'detection/detection-card/content/attachements'
+import { CreationAt } from 'detection/detection-card/content/items/creation-at'
+import { Description } from 'detection/detection-card/content/items/description'
+import { Attachements } from 'detection/detection-card/content/items/attachements'
 import { Container } from 'detection/detection-card/content/container'
 
 export const Content = () => <Container>
