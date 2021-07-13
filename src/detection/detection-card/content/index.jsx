90c556aafe511b0cb7a1127c8c9e571018d7b75f
Codrut <constantinescu321@gmail.com>
2021-07-13 14:57:29 +0300

    [HER-17]Follow-up Change path to components

--- src/detection/detection-card/content/index.jsx
+++ src/detection/detection-card/content/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import { CreationAt } from 'detection/detection-card/content/items/creation-at'
+import { CreatedAt } from 'detection/detection-card/content/items/created-at'
 import { Description } from 'detection/detection-card/content/items/description'
-import { Attachements } from 'detection/detection-card/content/items/attachements'
+import { Attachments } from 'detection/detection-card/content/items/attachments'
 import { Container } from 'detection/detection-card/content/container'
 
 export const Content = () => <Container>
-  <CreationAt />
+  <CreatedAt />
   <Description />
-  <Attachements />
+  <Attachments />
 </Container>
