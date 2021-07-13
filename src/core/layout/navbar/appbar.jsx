fef7aef98195612beec44637b6b588899e3b03bf
casian <casian.platon@ulbsibiu.ro>
2021-07-13 15:27:27 +0300

    [HER-40]Made navbar sticky

--- src/core/layout/navbar/appbar.jsx
+++ src/core/layout/navbar/appbar.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 
 import { useStyles } from 'core/layout/navbar/style'
 
-export const AppBar = ({ children }) => <MaterialAppBar position={'static'}>
+export const AppBar = ({ children }) => <MaterialAppBar position={'sticky'}>
   <Grid container className={useStyles().root} alignItems={'center'} justify={'space-between'}>
     {children}
   </Grid>
