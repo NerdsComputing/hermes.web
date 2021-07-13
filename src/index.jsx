b290ebcea1298763056f8aa6a820e22e7f2eba41
casian <casian.platon@ulbsibiu.ro>
2021-07-13 13:29:42 +0300

    [HER-14]Setup Apollo client

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+  ApolloClient, gql,
+  InMemoryCache
+} from '@apollo/client'
 
 import { App } from 'app'
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache(),
+})
+
+client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result))
 
 ReactDOM.render(<React.StrictMode>
   <App />
