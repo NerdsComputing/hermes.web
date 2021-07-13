ee8e95963efa06174d8239becf94663a2826228b
casian <casian.platon@ulbsibiu.ro>
2021-07-13 14:50:27 +0300

    [HER-14]Follow-up Change url and remove client

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {
-  ApolloClient, gql,
+  ApolloClient,
   InMemoryCache
 } from '@apollo/client'
 
 import { App } from 'app'
 
-const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+ApolloClient({
+  uri: 'https://hermes.api.dev.k8s.nerds.sh/',
   cache: new InMemoryCache(),
 })
-
-client
-  .query({
-    query: gql`
-      query GetRates {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-    `,
-  })
-  .then(result => console.log(result))
 
 ReactDOM.render(<React.StrictMode>
   <App />
