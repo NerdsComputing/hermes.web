0ed6b512fe5649a981b9ff933aa479f4af5d0389
Codrut <constantinescu321@gmail.com>
2021-07-13 12:39:51 +0300

    [HER-17]Follow-up remove empty items

--- src/detection/container.jsx
+++ src/detection/container.jsx
@@ -6,11 +6,7 @@ import { useStyles } from 'detection/style'
 
 export const DetectionWrapper = ({ children }) => <Grid container justify={'center'} alignContent={'center'}
                                                         className={useStyles().root}>
-  <Grid item xs={0} sm={1} md={2} lg={3} xl={4} />
-  <Grid container item xs={12} sm={8} md={8} lg={6} xl={4} justify={'center'}>
-    {children}
-  </Grid>
-  <Grid item xs={0} sm={1} md={2} lg={3} xl={4} />
+  {children}
 </Grid>
 
-DetectionWrapper.propTypes = { children: PropTypes.array.isRequired }
+DetectionWrapper.propTypes = { children: PropTypes.element.isRequired }
