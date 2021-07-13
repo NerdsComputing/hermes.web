3ed0883144ac8429cc14820d2706f61e9e9e4fae
casian <casian.platon@ulbsibiu.ro>
2021-07-13 15:58:40 +0300

    [HER-14]Follow-up Client file, remove from index

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {
-  ApolloClient,
-  InMemoryCache
-} from '@apollo/client'
 
 import { App } from 'app'
-
-ApolloClient({
-  uri: 'https://hermes.api.dev.k8s.nerds.sh/',
-  cache: new InMemoryCache(),
-})
 
 ReactDOM.render(<React.StrictMode>
   <App />
