66a09a750ba37b3fb023f1aefda6c452fe7f5f06
Codrut <constantinescu321@gmail.com>
2021-07-13 14:49:59 +0300

    [HER-17]Follow-up Create separate styles

---
+++ src/detection/detection-card/desktop-style.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  width: '50vw',
+  height: '60vh',
+  borderRadius: '50px',
+}
+
+export const useDesktopStyle = makeStyles({ root })
---
+++ src/detection/detection-card/mobile-style.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  width: '90vw',
+  height: '60vh',
+  borderRadius: '50px',
+}
+
+export const useMobileStyle = makeStyles({ root })
