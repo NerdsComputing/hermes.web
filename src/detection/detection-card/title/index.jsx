81af10c7d1f134760047cc9e65430bf76ac5a699
Codrut <constantinescu321@gmail.com>
2021-07-13 13:01:34 +0300

    [HER-17]Follow-up Make one liner

--- src/detection/detection-card/title/index.jsx
+++ src/detection/detection-card/title/index.jsx
@@ -3,5 +3,4 @@ import { CardHeader } from '@material-ui/core'
 
 import { useStyles } from 'detection/detection-card/title/style'
 
-export const Title = () => <CardHeader title={'Detection name'}
-                                       className={useStyles().root} />
+export const Title = () => <CardHeader title={'Detection name'} className={useStyles().root} />
