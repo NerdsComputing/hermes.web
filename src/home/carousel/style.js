8c872f6fa3d9f121c908f9f95fc9c3bfb4553dac
casian <casian.platon@ulbsibiu.ro>
2021-07-13 12:09:27 +0300

    [HER-40]Follow-up Add padding to carousel

--- src/home/carousel/style.js
+++ src/home/carousel/style.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core'
 const carousel = {
   textAlign: 'center',
   paddingTop: '2vh',
+  paddingBottom: '4vh',
   display: 'flex',
   justifyContent: 'center',
   flexDirection: 'row',
