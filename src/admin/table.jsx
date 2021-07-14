392d21faa2711962ee8d71f6aec09c667fe87a51
bianca <bia.topircean@gmail.com>
2021-07-14 16:15:59 +0300

    [HER-16] Follow-up Add alias for Table

--- src/admin/table.jsx
+++ src/admin/table.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Grid, Paper, Table, TableContainer } from '@material-ui/core'
+import { Grid, Paper, Table as MaterialTable , TableContainer } from '@material-ui/core'
 
 import { Head } from 'admin/head'
 import { User } from 'admin/user/user'
 
 export const Table = () => <Grid container justify={'center'}>
   <TableContainer component={Paper} style={{ maxWidth: '80%' }}>
-    <Table>
+    <MaterialTable>
       <Head />
       <User />
-    </Table>
+    </MaterialTable>
   </TableContainer>
 </Grid>
