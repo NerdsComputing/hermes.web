dbdfcaf07cbadfb4593caf3984c4cd8aa7d97909
bianca <bia.topircean@gmail.com>
2021-07-14 16:54:18 +0300

    [HER-16] Follow-up add a new line in style

--- src/admin/components/styled-row/style.js
+++ src/admin/components/styled-row/style.js
@@ -1,4 +1,5 @@
 import { makeStyles } from '@material-ui/core'
 
 const style = theme => ({ '&:nth-of-type(odd)': { backgroundColor: theme.palette.action.hover } })
+
 export const useStyles = makeStyles(theme => ({ root: { ...style(theme) } }))
