8542dd7bfe39d98b57169ef039d4d764b7758a02
bianca <bia.topircean@gmail.com>
2021-07-14 17:29:28 +0300

    [HER-16] Follow-up Rename user to body

--- src/admin/body/index.jsx
+++ src/admin/body/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { TableBody } from '@material-ui/core'
 
-import { users } from 'admin/user/users'
+import { users } from 'admin/body/users'
 import { Row } from 'admin/row'
 
 export const Body = () => <TableBody>
--- src/admin/table.jsx
+++ src/admin/table.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Grid, Paper, Table as MaterialTable, TableContainer } from '@material-ui/core'
 
 import { Head } from 'admin/head'
-import { Body } from 'admin/user'
+import { Body } from 'admin/body'
 
 export const Table = () => <Grid container justify={'center'}>
   <TableContainer component={Paper} style={{ maxWidth: '80%' }}>
