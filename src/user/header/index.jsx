280f8d75f5e33ba1c2b58dfb75b4f3d6a6b525fa
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-07-14 15:32:49 +0300

    [HER-15]Follow-up Put back the header components

--- src/user/header/index.jsx
+++ src/user/header/index.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
-import { Grid, Paper } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 
-import { items } from 'user/header/items'
-import { Components } from 'user/header/component'
-import { useStyle } from 'user/header/style'
+import { TableHeader } from 'user/header/table-header'
+import { Title } from 'user/header/title'
 
 export const Header = () => <Grid container spacing={2}>
-  <Grid item xs={12}>
-    <Paper className={useStyle().title}>Detections</Paper>
-  </Grid>
-  {
-    items.map((items, index) => <Components items={items} key={index} />)
-  }
+  <Title />
+  <TableHeader />
 </Grid>
 
