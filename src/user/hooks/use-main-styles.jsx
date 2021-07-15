d863b217ad02a205ba8db3d4d864dd6eec36846c
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-07-15 18:08:36 +0300

    [HER-15]Follow-up Independent style for main page

---
+++ src/user/hooks/use-main-styles.jsx
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  marginTop: '5vh',
+  maxWidth: '80%',
+  margin: 'auto',
+  paddingBottom: '10vh',
+}
+
+export const useMainStyles = makeStyles({ root })
