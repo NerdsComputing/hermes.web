15131341c300c57803ed8ae3edfb847c1eef3269
Codrut <constantinescu321@gmail.com>
2021-07-15 13:21:18 +0300

    [HER-18]Add /user path

--- src/startup/routes.jsx
+++ src/startup/routes.jsx
@@ -1,10 +1,15 @@
 import { HomePage } from 'home'
 import { About } from 'about'
+import { UserPage } from 'user'
 
 export const routes = [{
 
   path: '/about',
   Component: About,
+},
+{
+  path: '/user',
+  Component: UserPage,
 },
 {
   path: '/',
