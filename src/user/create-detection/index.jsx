819d6623880b3f272d51548f2d9ca90fd64f5771
Codrut <constantinescu321@gmail.com>
2021-07-15 13:08:54 +0300

    [HER-18]Create component for dialog

---
+++ src/user/create-detection/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Wrapper } from 'user/create-detection/wrapper'
+import { Title } from 'user/create-detection/title'
+import { Content } from 'user/create-detection/content'
+import { Action } from 'user/create-detection/action'
+
+export const CreateDetection = () => <Wrapper>
+  <Title />
+  <Content />
+  <Action />
+</Wrapper>
