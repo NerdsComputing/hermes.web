9673ebfc9c610feeffb46d20719205742f607e86
Codrut <constantinescu321@gmail.com>
2021-07-15 13:20:27 +0300

    [HER-18]Create action style

---
+++ src/user/create-detection/action/style.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  padding: '15px 0 15px 0',
+  justifyContent: 'space-around',
+  borderTop: '.5px solid black',
+}
+
+export const useStyles = makeStyles({ root })
