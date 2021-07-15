558e15a5955a9b6bb9dbe6fa82c243cc324c2f90
Codrut <constantinescu321@gmail.com>
2021-07-15 13:15:07 +0300

    [HER-18]Create description component

---
+++ src/user/create-detection/content/items/description.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import DescriptionIcon from '@material-ui/icons/Description'
+
+import { TextField } from 'user/create-detection/content/items/text-field'
+
+export const Description = () => <TextField text={'Description'} icon={<DescriptionIcon />} />
