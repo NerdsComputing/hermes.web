f75e9bb63e9f9550a2008f0f880b55609de32fe5
Codrut <constantinescu321@gmail.com>
2021-07-15 13:14:48 +0300

    [HER-18]Create attachments component

---
+++ src/user/create-detection/content/items/attachments.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import AttachmentIcon from '@material-ui/icons/Attachment'
+
+import { TextField } from 'user/create-detection/content/items/text-field'
+
+export const Attachments = () => <TextField text={'Attachments'} icon={<AttachmentIcon />} />
