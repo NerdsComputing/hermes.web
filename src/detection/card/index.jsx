e4c15c1a3f6ee18c6c10cb1559bdd61edc20030b
Codrut <constantinescu321@gmail.com>
2021-07-15 15:09:33 +0300

    [HER-17]Follow-up Rename import Buttons

--- src/detection/card/index.jsx
+++ src/detection/card/index.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 import { CardWrapper } from 'detection/card/card-wrapper'
 import { Title } from 'detection/card/title'
 import { Content } from 'detection/card/content'
-import { CardButtons } from 'detection/card/action'
+import { Buttons } from 'detection/card/action'
 
 export const Card = () => <CardWrapper>
   <Title />
   <Content />
-  <CardButtons />
+  <Buttons />
 </CardWrapper>
