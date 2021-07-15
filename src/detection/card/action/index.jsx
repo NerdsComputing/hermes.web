159d416daaa410c6e72f69603683bb18ba911ffc
Codrut <constantinescu321@gmail.com>
2021-07-15 15:11:17 +0300

    [HER-17]Follow-up Rename component

--- src/detection/card/action/index.jsx
+++ src/detection/card/action/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Button } from '@material-ui/core'
 
-import { CardAction } from 'detection/card/action/container'
+import { Actions } from 'detection/card/action/actions'
 
-export const CardButtons = () => <CardAction>
+export const Buttons = () => <Actions>
   <Button variant={'contained'} color={'primary'}>Set status</Button>
   <Button variant={'contained'} color={'primary'}>Edit</Button>
   <Button variant={'contained'} color={'primary'}>Delete</Button>
-</CardAction>
+</Actions>
