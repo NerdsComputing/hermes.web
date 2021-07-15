144bc9946a8819dbd181269c04201c8801c41d8b
Codrut <constantinescu321@gmail.com>
2021-07-15 15:22:29 +0300

    [HER-17]Follow-up Use wrapper component

--- src/detection/card/index.jsx
+++ src/detection/card/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import { CardWrapper } from 'detection/card/card-wrapper'
+import { Wrapper } from 'detection/card/wrapper'
 import { Title } from 'detection/card/title'
 import { Content } from 'detection/card/content'
 import { Buttons } from 'detection/card/action'
 
-export const Card = () => <CardWrapper>
+export const Card = () => <Wrapper>
   <Title />
   <Content />
   <Buttons />
-</CardWrapper>
+</Wrapper>
