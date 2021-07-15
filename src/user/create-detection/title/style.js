6372eac788fe438b3cc8cacdb86907470809db3d
Codrut <constantinescu321@gmail.com>
2021-07-15 13:10:10 +0300

    [HER-18]Create style for title

---
+++ src/user/create-detection/title/style.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  padding: '10px 100px 10px 100px',
+  borderBottom: '.5px solid black',
+  textAlign: 'center',
+}
+
+export const useStyles = makeStyles({ root })
