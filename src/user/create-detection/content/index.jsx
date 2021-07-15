eb0db6f9e5e8f688b73e369cc1e153bf11f4b0ab
Codrut <constantinescu321@gmail.com>
2021-07-15 13:11:14 +0300

    [HER-18]Create title component

---
+++ src/user/create-detection/content/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import { DialogContent } from 'user/create-detection/content/dialog-content'
+import { Items } from 'user/create-detection/content/items'
+
+export const Content = () => <DialogContent>
+  <Items />
+</DialogContent>
