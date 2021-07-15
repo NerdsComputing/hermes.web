209271cfe5d6d6bad3f1af41544e5133840b69ef
Codrut <constantinescu321@gmail.com>
2021-07-15 13:14:30 +0300

    [HER-18]Create task-name component

--- src/user/create-detection/content/items/taks-name.jsx
+++ src/user/create-detection/content/items/taks-name.jsx
@@ -3,4 +3,4 @@ import AssignmentIcon from '@material-ui/icons/Assignment'
 
 import { TextField } from 'user/create-detection/content/items/text-field'
 
-export const TaskName = () => <TextField text={'Task name'} icon={<AssignmentIcon />} />
+export const TaskName = () => <TextField text={'Task Name'} icon={<AssignmentIcon />} />
