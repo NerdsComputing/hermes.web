787f19b21796ffe2d68eb04ccb61036fb16a933f
Codrut <constantinescu321@gmail.com>
2021-07-16 12:33:03 +0300

    [HER-17]Follow-up Change components path

--- src/detection/card/action/index.jsx
+++ src/detection/card/action/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 import { Actions } from 'detection/card/action/actions'
-import { Delete } from 'detection/card/action/buttons/delete'
-import { SetStatus } from 'detection/card/action/buttons/set-status'
-import { Edit } from 'detection/card/action/buttons/edit'
+import { Delete } from 'detection/card/action/delete-button'
+import { SetStatus } from 'detection/card/action/set-status-button'
+import { Edit } from 'detection/card/action/edit-button'
 
 export const Buttons = () => <Actions>
   <SetStatus />
