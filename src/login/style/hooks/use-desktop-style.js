b1f014a8895363f9964abd969e40e03ab8297cc0
casian <casian.platon@ulbsibiu.ro>
2021-07-16 13:11:05 +0300

    [HER-12]Desktop styling

---
+++ src/login/style/hooks/use-desktop-style.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  width: '8vw',
+  height: '3vh',
+  borderRadius: '40px',
+  marginRight: '15px',
+  backgroundColor: '#FFFF',
+}
+
+export const useDesktopStyles = makeStyles({ root })
