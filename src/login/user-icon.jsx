6754d41dbd1d2ea083f8a1d6c3086d6bf26dceb5
casian <casian.platon@ulbsibiu.ro>
2021-07-16 13:14:15 +0300

    [HER-12]Change image when logged in or logged out

---
+++ src/login/user-icon.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PersonIcon from '@material-ui/icons/Person'
+import * as PropTypes from 'prop-types'
+
+import { ProfileImage } from 'login/profile-image'
+
+export const UserIcon = ({ user }) => {
+  console.log(user)
+
+  return user ? <ProfileImage user={user} /> : <PersonIcon />
+}
+
+UserIcon.propTypes = { user: PropTypes.object }
+UserIcon.defaultProps = { user: undefined }
