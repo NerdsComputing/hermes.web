fd6edfa137c6dbccfeaf1b945a2280939bc5e30b
bianca <bia.topircean@gmail.com>
2021-07-16 16:30:06 +0300

    [HER-16] Reorder imports

--- src/admin/row/about/email.jsx
+++ src/admin/row/about/email.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid, Typography } from '@material-ui/core'
 import PropTypes from 'prop-types'
+
 import { useStyles } from 'admin/row/about/style'
 
 export const Email = ({ user }) => <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
--- src/admin/row/about/id-number.jsx
+++ src/admin/row/about/id-number.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid, Typography } from '@material-ui/core'
 import PropTypes from 'prop-types'
+
 import { useStyles } from 'admin/row/about/style'
 
 export const IdNumber = ({ user }) => <Grid container className={useStyles().alignment}>
