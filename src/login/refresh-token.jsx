66ac08c30cacdf67bea06f2b5c4137400c305c98
casian <casian.platon@ulbsibiu.ro>
2021-07-16 13:13:06 +0300

    [HER-12]Refresh token for google login

---
+++ src/login/refresh-token.jsx
@@ -0,0 +1,11 @@
+const refreshToken = async(handle, refreshTiming) => {
+  const newAuthRes = await handle.reloadAuthResponse()
+  refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000
+  setTimeout(refreshToken, refreshTiming)
+}
+
+export const Refreshtoken = handle => {
+  const refreshTiming = (handle.tokenObj.expires_in || 3600 - 5 * 60) * 1000
+  refreshToken(handle, refreshTiming)
+  setTimeout(refreshToken, refreshTiming)
+}
