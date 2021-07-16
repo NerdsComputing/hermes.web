68196a8a3e364c41b5e07fa28d77fb27d1efed1f
casian <casian.platon@ulbsibiu.ro>
2021-07-16 13:11:21 +0300

    [HER-12]Mobile styling

---
+++ src/login/style/hooks/use-mobile-style.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  width: '29vw',
+  height: '4vh',
+  borderRadius: '40px',
+  marginRight: '15px',
+  backgroundColor: '#FFFF',
+}
+
+export const useMobileStyles = makeStyles({ root })
