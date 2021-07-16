61efef6fd08864e95e444131a58d948430f330f8
casian <casian.platon@ulbsibiu.ro>
2021-07-16 12:54:40 +0300

    [HER-12]Add login component to navbar

--- src/core/layout/navbar/index.jsx
+++ src/core/layout/navbar/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 import { Title } from 'core/layout/navbar/title'
-import { UserContent } from 'core/layout/navbar/content'
 import { AppBar } from 'core/layout/navbar/appbar'
+import { Login } from 'login/login'
 
 export const Navbar = () => <AppBar>
   <Title />
-  <UserContent />
+  <Login />
 </AppBar>
