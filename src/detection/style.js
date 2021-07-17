45b74f10110f9ec309c688a599140f64d7c3ae64
Codrut <constantinescu321@gmail.com>
2021-07-17 13:08:37 +0300

    [HER-17]Follow-up Remove background-color

--- src/detection/style.js
+++ src/detection/style.js
@@ -2,8 +2,7 @@ import { makeStyles } from '@material-ui/core'
 
 const root = {
   width: '100%',
-  height: '100vh',
-  backgroundColor: '#d4fae3',
+  height: '90vh',
 }
 
 export const useStyles = makeStyles({ root })
