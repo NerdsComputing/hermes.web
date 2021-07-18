d1e7a13a3be6c821a9867f2276666d0d90f70880
casian <casian.platon@ulbsibiu.ro>
2021-07-18 14:36:42 +0300

    [HER-12]Follow-up Extract style to object

--- src/login/style/hooks/use-desktop-style.js
+++ src/login/style/hooks/use-desktop-style.js
@@ -1,11 +1,15 @@
 import { makeStyles } from '@material-ui/core'
 
-const root = {
-  width: '8vw',
-  height: '3vh',
+const buttonstyle = {
   borderRadius: '40px',
   marginRight: '15px',
   backgroundColor: '#FFFF',
+}
+
+const root = {
+  width: '8vw',
+  height: '3vh',
+  ...buttonstyle,
 }
 
 export const useDesktopStyles = makeStyles({ root })
--- src/login/style/hooks/use-mobile-style.js
+++ src/login/style/hooks/use-mobile-style.js
@@ -1,11 +1,15 @@
 import { makeStyles } from '@material-ui/core'
 
-const root = {
-  width: '29vw',
-  height: '4vh',
+const buttonstyle = {
   borderRadius: '40px',
   marginRight: '15px',
   backgroundColor: '#FFFF',
+}
+
+const root = {
+  width: '29vw',
+  height: '4vh',
+  ...buttonstyle,
 }
 
 export const useMobileStyles = makeStyles({ root })
