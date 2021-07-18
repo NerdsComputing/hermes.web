ba4177a52c6beea7cb31ebba255e4c1b4bb4eab3
casian <casian.platon@ulbsibiu.ro>
2021-07-18 14:33:28 +0300

    [HER-12]Follow-up Rename to .js

--- src/logout/logout-config.js
+++ src/logout/logout-config.js
@@ -1,14 +1,12 @@
 import { useCallback } from 'react'
 
 import { useContext } from 'login/context'
-
-export const clientId = '887902106704-jk4r0u9rkeu74mkd6sq6957qmpj51h1c.apps.googleusercontent.com'
+import { clientId } from 'login/config'
 
 export const useSuccess = () => {
   const { setUser } = useContext()
 
   return useCallback(handle => {
-    console.log('[Logged out]', handle)
     setUser(undefined)
   }, [setUser])
 }
