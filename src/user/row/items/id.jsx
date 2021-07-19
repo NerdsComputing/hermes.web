27bdc90afa8908696c5b1ef97e436fa627a950d8
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-07-19 08:42:14 +0300

    [HER-15]Use complex data in id

--- src/user/row/items/id.jsx
+++ src/user/row/items/id.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Grid, Paper } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 import PropTypes from 'prop-types'
 
 import { usePaperStyles } from 'user/row/hooks/use-paper-styles'
 
-export const Id = ({ id }) => <Grid item xs={3}>
-  <Paper classes={usePaperStyles()}>{id}</Paper>
-</Grid>
+export const Id = ({ id }) => <Grid item xs={3} classes={usePaperStyles()}>{id}</Grid>
 
 Id.propTypes = { id: PropTypes.number.isRequired }
