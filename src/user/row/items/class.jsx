a7823a5d117efb2a1e769e6555bac115df2cf9aa
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-07-19 08:42:06 +0300

    [HER-15]Use complex data in class

--- src/user/row/items/class.jsx
+++ src/user/row/items/class.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Grid, Paper } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 
 import { usePaperStyles } from 'user/row/hooks/use-paper-styles'
+import PropTypes from 'prop-types'
 
-export const Class = () => <Grid item xs={3}>
-  <Paper classes={usePaperStyles()}>detection_class</Paper>
-</Grid>
+export const Class = ({ theClass }) => <Grid item xs={3} classes={usePaperStyles()}>{theClass}</Grid>
 
+Class.propTypes = { theClass: PropTypes.string.isRequired }
