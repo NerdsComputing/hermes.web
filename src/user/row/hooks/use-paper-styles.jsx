a789431829774ba3531c1ddf84aab2d0b4df923d
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-07-19 08:43:05 +0300

    [HER-15]Make style fot the components

--- src/user/row/hooks/use-paper-styles.jsx
+++ src/user/row/hooks/use-paper-styles.jsx
@@ -1,13 +1,11 @@
 import { makeStyles } from '@material-ui/core'
 
 const root = {
-  width: '19.5vw',
+  width: '19vw',
   paddingTop: '1vh',
   paddingBottom: '1vh',
   textAlign: 'center',
   fontSize: '1vw',
-  backgroundColor: 'transparent',
-  boxShadow: 'none',
 }
 
 export const usePaperStyles = makeStyles({ root })
