b6e2f05e7b4f4c8c1c2bc1cd9efa8ca916621089
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-07-19 08:40:53 +0300

    [HER-15]Follow-up Array hold more complex data

---
+++ src/user/items.js
@@ -0,0 +1,43 @@
+export const items = [
+  {
+    id: 1,
+    score: 50,
+    class: 'ceva',
+    timeStamp: '05-07-2000',
+  },
+  {
+    id: 1,
+    score: 50,
+    class: 'ceva',
+    timeStamp: '05-07-2000',
+  }, {
+    id: 1,
+    score: 50,
+    class: 'ceva',
+    timeStamp: '05-07-2000',
+  }, {
+    id: 1,
+    score: 50,
+    class: 'ceva',
+    timeStamp: '05-07-2000',
+  }, {
+    id: 1,
+    score: 50,
+    class: 'ceva',
+    timeStamp: '05-07-2000',
+  }, {
+    id: 1,
+    score: 50,
+    class: 'ceva',
+    timeStamp: '05-07-2000',
+  }, {
+    id: 1,
+    score: 50,
+    class: 'ceva',
+    timeStamp: '05-07-2000',
+  }, {
+    id: 1,
+    score: 50,
+    class: 'ceva',
+    timeStamp: '05-07-2000',
+  }]
