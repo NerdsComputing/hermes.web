a0e8cb533202df5a0d0cc9f255f85b1f7c518f70
TBiannca <58395222+TBiannca@users.noreply.github.com>
2021-08-03 16:06:01 +0300

    [HER-114] Create detections route (#25)
    * [HER-114] Add /detections route
    
    * [HER-114] Create Detections component

---
+++ src/detections/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+
+export const Detections = () => <div>Put components here</div>
+
--- src/startup/routes.jsx
+++ src/startup/routes.jsx
@@ -2,22 +2,27 @@ import { HomePage } from 'home'
 import { DetectionPage } from 'detection'
 import { About } from 'about'
 import { Admin } from 'admin'
+import { Detections } from 'detections'
 
-export const routes = [
-  {
-    path: '/detection/:id',
-    component: DetectionPage,
-  },
-  {
-    path: '/admin',
-    Component: Admin,
-  },
-  {
+export const routes = [{
 
-    path: '/about',
-    component: About,
-  },
-  {
-    path: '/',
-    component: HomePage,
-  }]
+  path: '/detections',
+  Component: Detections,
+},
+{
+  path: '/detection/:id',
+  component: DetectionPage,
+},
+{
+  path: '/admin',
+  Component: Admin,
+},
+{
+
+  path: '/about',
+  component: About,
+},
+{
+  path: '/',
+  component: HomePage,
+}]
