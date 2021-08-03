c489312c4b6669c820ae3ce1f7210b6c001aa626
bianca <bia.topircean@gmail.com>
2021-08-03 19:15:07 +0300

    [HER-121] Create Buttons component

---
+++ src/home/buttons/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button, Grid } from '@material-ui/core'
+
+export const Buttons = () => <Grid container spacing={'10'} justify={'center'}>
+  <Grid item>
+    <Button size={'large'} variant={'contained'} color={'primary'}>Button</Button>
+  </Grid>
+  <Grid item>
+    <Button size={'large'} variant={'contained'} color={'primary'}>Button</Button>
+  </Grid>
+</Grid>
