7dc5062ad846547e6a6a3d7224b8b36243246085
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-03 17:15:55 +0300

    [HER-12]Follow-up Change import

--- src/core/layout/navbar/index.jsx
+++ src/core/layout/navbar/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Title } from 'core/layout/navbar/title'
 import { AppBar } from 'core/layout/navbar/appbar'
-import { Login } from 'login/login'
+import { Login } from 'login'
 
 export const Navbar = () => <AppBar>
   <Title />
