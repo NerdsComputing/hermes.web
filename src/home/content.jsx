4148998b84cf2e27e51a2595378f61b55f85a735
bianca <bia.topircean@gmail.com>
2021-08-03 19:15:25 +0300

    [HER-121] Add Buttons to Content

--- src/home/content.jsx
+++ src/home/content.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 import { Title } from 'home/title'
 import { Carousel } from 'home/carousel'
+import { Buttons } from 'home/buttons'
 
 export const Content = () => <>
   <Title />
   <Carousel />
+  <Buttons />
 </>
