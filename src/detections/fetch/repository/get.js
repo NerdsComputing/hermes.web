55d9c4c4835a432d93738dd55e0ba26ceb6266c2
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-04 18:02:21 +0300

    [HER-65]Follow-up Change client to .js and to be a function

--- src/detections/fetch/repository/get.js
+++ src/detections/fetch/repository/get.js
@@ -1,6 +1,6 @@
-import { client } from 'startup/client'
 import { from } from 'rxjs'
 
 import { GET_DETECTIONS } from 'detections/fetch/repository/query'
+import { Client } from 'startup/client'
 
-export const get = payload => from(client.query({ query: GET_DETECTIONS, variables: { parameter: payload } }))
+export const get = payload => from(Client().query({ query: GET_DETECTIONS, variables: { parameter: payload } }))
