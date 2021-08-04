603d295254b9b3da5e761ce4a8d77b0b0a0c61e0
Codrut <constantinescu321@gmail.com>
2021-08-04 16:24:17 +0300

    [HER-115]Follow-up Remove custom hook

--- src/cameras/index.jsx
+++ src/cameras/index.jsx
@@ -1,9 +1,3 @@
 import React from 'react'
 
-import { useGetCameras } from 'cameras/hooks/use-get-cameras'
-
-export const Cameras = () => {
-  useGetCameras()
-
-  return <>Cameras</>
-}
+export const Cameras = () => <>Cameras</>
