c248a29940a1f0aa6e7d8e3a405e6cf0028055c0
Codrut <constantinescu321@gmail.com>
2021-08-04 18:02:06 +0300

    [HER-115]Follow-up Rename client

--- src/cameras/repository/get.js
+++ src/cameras/repository/get.js
@@ -1,9 +1,9 @@
 import { from } from 'rxjs'
 
-import { Client } from 'startup/client'
+import { client } from 'startup/client'
 import { GET_CAMERAS } from 'cameras/repository/query'
 
-export const get = payload => from(Client().query({
+export const get = payload => from(client().query({
   query: GET_CAMERAS,
   variables: { parameter: payload },
 }))
--- src/startup/client.jsx
+++ src/startup/client.jsx
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 import { settings } from 'settings'
 
-export const Client = () => new ApolloClient({
+export const client = () => new ApolloClient({
   uri: settings().url,
   cache: new InMemoryCache(),
 })
