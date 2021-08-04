bc7a344cb3782a6dc1100be15143d86efd91695c
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-04 15:01:02 +0300

    [HER-65]Follow-up Rearrange the imports

--- src/detections/fetch/repository/get.js
+++ src/detections/fetch/repository/get.js
@@ -1,5 +1,6 @@
 import { client } from 'startup/client'
-import { GET_ALL_DETECTIONS } from 'detections/fetch/repository/query'
 import { from } from 'rxjs'
 
-export const get = payload => from(client.query({ query: GET_ALL_DETECTIONS, variables: { parameter: payload } }))
+import { GET_DETECTIONS } from 'detections/fetch/repository/query'
+
+export const get = payload => from(client.query({ query: GET_DETECTIONS, variables: { parameter: payload } }))
