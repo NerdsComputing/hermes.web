86c5e7d1bcacb45942df96808d44bba7491b3356
bianca <bia.topircean@gmail.com>
2021-08-04 10:44:18 +0300

    [HER-121] Change buttons names

--- src/home/buttons/index.jsx
+++ src/home/buttons/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Button, Grid } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
+import { CameraButton } from 'home/buttons/camera-button'
+import { DetectionButton } from 'home/buttons/detection-button'
 
 export const Buttons = () => <Grid container spacing={'10'} justify={'center'}>
-  <Grid item>
-    <Button size={'large'} variant={'contained'} color={'primary'}>Button</Button>
-  </Grid>
-  <Grid item>
-    <Button size={'large'} variant={'contained'} color={'primary'}>Button</Button>
-  </Grid>
+  <CameraButton>See Cameras</CameraButton>
+  <DetectionButton>See Detections</DetectionButton>
 </Grid>
