09b1cfacfc92a6e648db06506fc6f152f97f7d99
bianca <bia.topircean@gmail.com>
2021-08-04 14:18:43 +0300

    [HER-121] Follow-up Rename buttons folder

--- src/home/content.jsx
+++ src/home/content.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Title } from 'home/title'
 import { Carousel } from 'home/carousel'
-import { Live } from 'home/buttons'
+import { Live } from 'home/live'
 
 export const Content = () => <>
   <Title />
