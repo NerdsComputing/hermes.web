4bdf650b0c6a393afbc6163b75d6245bc789189d
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-04 13:30:20 +0300

    [HER-65]Add epics to root epics

--- src/startup/root-epic.js
+++ src/startup/root-epic.js
@@ -1,5 +1,6 @@
 import { combineEpics } from 'redux-observable'
 
+import { detections } from 'detections/fetch/epics'
 import { cameras } from 'cameras/epics'
 
-export const rootEpic = combineEpics(cameras)
+export const rootEpic = combineEpics(detections, cameras)
