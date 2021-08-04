446611ea51788f838c305b144b8c265f34200e84
bianca <bia.topircean@gmail.com>
2021-08-04 13:21:08 +0300

    [HER-121] Reorder imports

--- src/home/buttons/hooks/use-navigation.jsx
+++ src/home/buttons/hooks/use-navigation.jsx
@@ -1,5 +1,5 @@
-import { useHistory } from 'react-router-dom'
 import { useCallback } from 'react'
+import { useHistory } from 'react-router-dom'
 
 export const useNavigation = path => {
   const history = useHistory()
