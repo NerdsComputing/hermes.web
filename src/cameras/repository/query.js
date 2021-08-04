84a6aeb51ead75efbe77f2e3256374a081e25144
Codrut <constantinescu321@gmail.com>
2021-08-04 18:01:47 +0300

    [HER-115]Follow-up Format query

--- src/cameras/repository/query.js
+++ src/cameras/repository/query.js
@@ -2,14 +2,14 @@ import { gql } from '@apollo/client'
 
 export const GET_CAMERAS = gql`
    query($parameter: TPCamera!) {
-  cameras(parameter: $parameter){
-    totalCount
-    pageIndex
-    pageSize
-    items {
-      id
-      latitude
-      longitude
-  }
-  }
+    cameras(parameter: $parameter) {
+        totalCount
+        pageIndex
+        pageSize
+        items {
+            id
+            latitude
+            longitude
+        }
+    }
 }`
