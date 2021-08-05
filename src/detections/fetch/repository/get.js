6c11f3286a9e27e0c590bf15d39f8b2609d9266e
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-05 15:04:33 +0300

    [HER-65] Add map to repository

--- src/detections/fetch/repository/get.js
+++ src/detections/fetch/repository/get.js
@@ -1,6 +1,8 @@
 import { from } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 import { GET_DETECTIONS } from 'detections/fetch/repository/query'
 import { client } from 'startup/client'
 
 export const get = payload => from(client().query({ query: GET_DETECTIONS, variables: { parameter: payload } }))
+  .pipe(map(result => result.data.detections))
