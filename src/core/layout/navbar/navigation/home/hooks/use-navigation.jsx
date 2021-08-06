00490707dcb152d25f651c264ae86b7f9cf07ec0
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-06 14:52:25 +0300

    [HER-125] Navigation hook to homepage

---
+++ src/core/layout/navbar/navigation/home/hooks/use-navigation.jsx
@@ -0,0 +1,10 @@
+import { useCallback } from 'react'
+import { useHistory } from 'react-router-dom'
+
+export const useNavigation = () => {
+  const history = useHistory()
+
+  return useCallback(() => {
+    history.push('/')
+  }, [history])
+}
