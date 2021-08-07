0cadcd04e1a04a5a0516373d3354918da63ed956
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-07 19:45:17 +0300

    [HER-125] Follow-up Use classes prop

--- src/core/layout/navbar/header/index.jsx
+++ src/core/layout/navbar/header/index.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 
 import { useStyles } from 'core/layout/navbar/header/style'
-import { NavigateToDetections } from 'core/layout/navbar/navigation/detections/navigate-to-detections'
-import { NavigateToCameras } from 'core/layout/navbar/navigation/cameras/navigate-to-cameras'
-import { Home } from 'core/layout/navbar/navigation/home/home'
+import { Home } from 'core/layout/navbar/navigation/home'
+import { NavigateToDetections } from 'core/layout/navbar/navigation/detections'
+import { NavigateToCameras } from 'core/layout/navbar/navigation/cameras'
 
-export const Header = () => <Grid item xs={5} sm={5} md={5} lg={5} xl={5} className={useStyles().root}>
+export const Header = () => <Grid item xs={5} sm={5} md={5} lg={5} xl={5} classes={useStyles()}>
   <Home />
   <NavigateToDetections />
   <NavigateToCameras />
