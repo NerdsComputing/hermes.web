7ee3cda73553a74954ddcdd3d32767b690755c70
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-07 19:40:14 +0300

    [HER-125] Follow-up Rename to index

--- src/core/layout/navbar/navigation/home/home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Link } from '@material-ui/core'
-
-import { useStyles } from 'core/layout/navbar/navigation/home/style'
-import { useNavigation } from 'core/layout/navbar/navigation/home/hooks/use-navigation'
-
-export const Home = () => <Link onClick={useNavigation()} variant={'h6'} className={useStyles().root}>
-  Hermes
-</Link>
