6fdbfafbd5c3f3e9fdc7ea95c0829feca2161ab3
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-07 19:49:12 +0300

    [HER-125] Follow-up Change useNavigation hook

--- src/core/layout/navbar/navigation/hooks/use-navigation.js
+++ src/core/layout/navbar/navigation/hooks/use-navigation.js
@@ -1,10 +1,10 @@
 import { useCallback } from 'react'
 import { useHistory } from 'react-router-dom'
 
-export const useNavigation = () => {
+export const useNavigation = path => {
   const history = useHistory()
 
   return useCallback(() => {
-    history.push('/cameras')
-  }, [history])
+    history.push(path)
+  }, [history, path])
 }
