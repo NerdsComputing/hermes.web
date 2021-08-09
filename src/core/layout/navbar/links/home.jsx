2d7c660d8bade6bb1515d78c7950d8634082b4e8
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-09 12:07:47 +0300

    [HER-125] Follow-up Move to a new directory

--- src/core/layout/navbar/links/home.jsx
+++ src/core/layout/navbar/links/home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Link } from '@material-ui/core'
 
-import { useStyles } from 'core/layout/navbar/navigation/home/style'
-import { useNavigation } from 'core/layout/navbar/navigation/hooks/use-navigation'
+import { useNavigation } from 'core/layout/navbar/links/hooks/use-navigation'
+import { useStyles } from 'core/layout/navbar/links/styles'
 
 export const Home = () => <Link onClick={useNavigation('/')} variant={'h6'} classes={useStyles()}>
   Hermes
