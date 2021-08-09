d378c6dd2ceba2548664751c74d11681a87623cd
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-09 14:48:04 +0300

    [HER-125] Follow-up Add Box wrapper

--- src/home/live/navigation-button.jsx
+++ src/home/live/navigation-button.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Button, Grid } from '@material-ui/core'
+import { Box, Button, Grid } from '@material-ui/core'
 
 import { useNavigation } from 'home/live/hooks/use-navigation'
 
 export const NavigationButton = ({ children, path }) => <Grid item>
-  <Button variant={'contained'} color={'primary'} onClick={useNavigation(path)}>
-    {children}
-  </Button>
+  <Box px={5} py={1}>
+    <Button variant={'contained'} color={'primary'} onClick={useNavigation(path)}>
+      {children}
+    </Button>
+  </Box>
 </Grid>
 
 NavigationButton.propTypes = {
