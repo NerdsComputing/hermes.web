4fc378ec2b4b4bcdaff94763fd60eb09ca4f7442
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-09 12:07:52 +0300

    [HER-125] Follow-up Move to a new directory

--- src/core/layout/navbar/mobile/icon.jsx
+++ src/core/layout/navbar/mobile/icon.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { IconButton } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 
-import { useStyles } from 'core/layout/navbar/responsive-drawer/mobile/style'
-import { useProps } from 'core/layout/navbar/responsive-drawer/mobile/hooks/use-props'
+import { useStyles } from 'core/layout/navbar/mobile/style'
+import { useProps } from 'core/layout/navbar/mobile/hooks/use-props'
 
 export const Icon = () => <IconButton className={useStyles().menuButton} {...useProps()}>
   <MenuIcon />
