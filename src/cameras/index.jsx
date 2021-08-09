9e8cd9ac7bfb123ac8fd80fe12f9c485ac36a234
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-09 13:39:12 +0300

    [HER-126] Union of the components from camera

--- src/cameras/index.jsx
+++ src/cameras/index.jsx
@@ -1,3 +1,14 @@
 import React from 'react'
+import * as Material from '@material-ui/core'
 
-export const Cameras = () => <>Cameras</>
+import { Sidebar } from 'cameras/sidebar'
+import { Content } from 'cameras/content'
+import { useDefaultContext, Context } from 'cameras/context'
+
+export const Cameras = () => <Context.Provider value={useDefaultContext()}>
+  <Material.Grid container>
+    <Sidebar />
+    <Content />
+  </Material.Grid>
+</Context.Provider>
+
