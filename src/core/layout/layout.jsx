366aec5177b8050d7675b8a9b070b7f6ead31a86
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-09 14:47:30 +0300

    [HER-125] Follow-up Add Children component

--- src/core/layout/layout.jsx
+++ src/core/layout/layout.jsx
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types'
 
 import { Navbar } from 'core/layout/navbar'
 import { Footer } from 'core/layout/footer'
+import { Children } from 'core/layout/children'
 
 export const Layout = ({ children }) => <>
   <Navbar />
-  {children}
+  <Children>
+    {children}
+  </Children>
   <Footer />
 </>
 
