4d7636203dd976757f4edaec3111bace9ee16330
bianca <bia.topircean@gmail.com>
2021-08-09 11:43:26 +0300

    [HER-119] Create chart component

--- src/detections/index.jsx
+++ src/detections/index.jsx
@@ -1,3 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Bar } from 'react-chartjs-2'
+import { Grid } from '@material-ui/core'
 
-export const Detections = () => <div>Put components here</div>
+import { labels } from 'detections/labels.json'
+import { ByDays } from 'detections/by-days'
+
+const hours = state => Object.entries(state)
+  .map(([key, value]) => ({
+    label: key,
+    data: value,
+    backgroundColor: `#${((1 << 24) * Math.random() | 0).toString(16)}`,
+  }))
+
+const data = (state, label) => ({
+  labels: label,
+  datasets: hours(state),
+
+})
+
+const useData = (setState, byParameter) => useEffect(() => {
+  byParameter(setState)
+}, [])
+
+export const Detections = () => {
+  const [state, setState] = useState({})
+  useData(setState, ByDays)
+
+  return <Grid item>
+    <Bar width={'500px'} height={'500px'}
+         options={{ maintainAspectRatio: false }} data={data(state, labels.days)} />
+  </Grid>
+}
+
