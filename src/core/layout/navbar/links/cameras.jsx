328f46b39ea7be2dc945da8a3b2e5604c3713c42
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-09 12:06:56 +0300

    [HER-125] Follow-up Move to a new directory

--- src/core/layout/navbar/links/cameras.jsx
+++ src/core/layout/navbar/links/cameras.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Link } from '@material-ui/core'
 
-import { useStyles } from 'core/layout/navbar/navigation/cameras/styles'
-import { useNavigation } from 'core/layout/navbar/navigation/hooks/use-navigation'
+import { useNavigation } from 'core/layout/navbar/links/hooks/use-navigation'
+import { useStyles } from 'core/layout/navbar/links/styles'
 
 export const NavigateToCameras = () => <Link onClick={useNavigation('/cameras')} variant={'h6'} classes={useStyles()}>
   Cameras
