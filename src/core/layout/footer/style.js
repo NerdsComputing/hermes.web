ec516286048aae7a0211ee62dfcb93d5f3625927
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-09 14:43:35 +0300

    [HER-126] Follow-up Adjust the footer

--- src/core/layout/footer/style.js
+++ src/core/layout/footer/style.js
@@ -1,14 +1,11 @@
 import { makeStyles } from '@material-ui/core'
 
-const position = theme => ({
-  width: '100%',
-})
+const position = ({ width: '100%' })
 
 export const useStyles = makeStyles(theme => ({
   root: {
     backgroundColor: theme.palette.primary.main,
-    ...position(theme),
+    ...position,
     minHeight: '5vh',
-    height: '5vh',
   },
 }))
