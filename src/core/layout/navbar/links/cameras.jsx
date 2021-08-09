d3e3e23bc1941df8e9f0cf214a76834394d147e9
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-09 14:44:02 +0300

    [HER-125] Follow-up Add Grid and Box wrappers

--- src/core/layout/navbar/links/cameras.jsx
+++ src/core/layout/navbar/links/cameras.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Link } from '@material-ui/core'
+import * as Material from '@material-ui/core'
 
-import { useNavigation } from 'core/layout/navbar/links/hooks/use-navigation'
 import { useStyles } from 'core/layout/navbar/links/styles'
+import { useNavigation } from 'core/layout/navbar/links/hooks/use-navigation'
 
-export const NavigateToCameras = () => <Link onClick={useNavigation('/cameras')} variant={'h6'} classes={useStyles()}>
-  Cameras
-</Link>
+export const NavigateToCameras = () => <Material.Grid item>
+  <Material.Box px={2}>
+    <Material.Link onClick={useNavigation('/cameras')} variant={'h6'} classes={useStyles()}>
+      Cameras
+    </Material.Link>
+  </Material.Box>
+</Material.Grid>
