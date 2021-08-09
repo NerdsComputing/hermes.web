ea58174bd761bb0216649eaaacfae58d725fbd45
bianca <bia.topircean@gmail.com>
2021-08-09 11:42:33 +0300

    [HER-119] Filter data by hours

---
+++ src/detections/by-hours.jsx
@@ -0,0 +1,37 @@
+import { from, reduce } from 'rxjs'
+import { mergeMap, tap } from 'rxjs/operators'
+
+import { data } from 'detections/data.json'
+
+const countDetections = value => {
+  const array = [[], [], [], []]
+  value.map(entry => array[Math.floor(new Date(entry.timestamp).getHours() / 6)].push(entry))
+
+  return array.map(entry => entry.length)
+}
+
+const addToClass = (acc, current) => {
+  acc[current.class].push(current)
+
+  return acc
+}
+
+const addClass = (acc, current) => {
+  acc[current.class] = [current]
+
+  return acc
+}
+
+const accumulator = (acc, [key, value]) => {
+  acc[key] = countDetections(value)
+
+  return acc
+}
+
+export const ByHours = setState => from(data.detections.items)
+  .pipe(reduce((acc, current) => acc[current.class] ? addToClass(acc, current) : addClass(acc, current), {}))
+  .pipe(tap(acc => console.log(acc)))
+  .pipe(mergeMap(data => Object.entries(data)))
+  .pipe(reduce(accumulator, {}))
+  .pipe(tap(acc => console.log(acc)))
+  .subscribe(acc => setState(acc))
