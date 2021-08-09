0e94333e5248124de6cb4552c8252f2f7a67b245
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-09 15:54:12 +0300

    [HER-126] Follow-up Make props for checkbox

--- src/cameras/list/items/item.jsx
+++ src/cameras/list/items/item.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
 import { Checkbox, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
+
+import { useProps } from 'cameras/list/hooks/use-props'
 
 export const Item = ({ item }) => <ListItem>
   <ListItemIcon>
-    <Checkbox color={'primary'}
-              inputProps={{ 'aria-label': 'secondary checkbox' }} />
+    <Checkbox {...useProps()} />
   </ListItemIcon>
   <ListItemText primary={item} />
 </ListItem>
