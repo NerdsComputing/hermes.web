54b508b489c7cfc7e1302ef2d855a12713875405
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-09 12:53:07 +0300

    [HER-65]Follow-up Adding some blank spaces

--- src/startup/root-reducer.js
+++ src/startup/root-reducer.js
@@ -3,4 +3,4 @@ import { combineReducers } from '@reduxjs/toolkit'
 import { detections } from 'detections/fetch/slices'
 import { cameras } from 'cameras/slices'
 
-export const rootReducer = combineReducers({ detections , cameras})
+export const rootReducer = combineReducers({ detections, cameras })
