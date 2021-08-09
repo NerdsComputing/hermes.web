d850207095c1ec81c9da0a203aaac1af27256849
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-09 12:08:32 +0300

    [HER-125] Follow-up Update imports

--- src/core/layout/navbar/header/index.jsx
+++ src/core/layout/navbar/header/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 
+import { Home } from 'core/layout/navbar/links/home'
 import { useStyles } from 'core/layout/navbar/header/style'
-import { Home } from 'core/layout/navbar/navigation/home'
-import { NavigateToDetections } from 'core/layout/navbar/navigation/detections'
-import { NavigateToCameras } from 'core/layout/navbar/navigation/cameras'
+import { NavigateToDetections } from 'core/layout/navbar/links/detections'
+import { NavigateToCameras } from 'core/layout/navbar/links/cameras'
 
 export const Header = () => <Grid item xs={5} sm={5} md={5} lg={5} xl={5} classes={useStyles()}>
   <Home />
