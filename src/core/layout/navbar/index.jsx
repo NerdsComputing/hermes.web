d3d29c5b5baef616ac004ec728ac2a5ecfeeb7cb
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-09 12:41:33 +0300

    [HER-125] Follow-up Update imports

--- src/core/layout/navbar/index.jsx
+++ src/core/layout/navbar/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { useDefaultContext, Context } from 'core/layout/navbar/context'
 import { DesktopNavbar } from 'core/layout/navbar/desktop'
-import { MobileNavbar } from 'core/layout/navbar/mobile/mobile'
+import { MobileNavbar } from 'core/layout/navbar/mobile'
 
 export const Navbar = () => <Context.Provider value={useDefaultContext()}>
   <DesktopNavbar />
