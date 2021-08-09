67a40c7ab7cc1bd484b68b7f78d57064ba87e4d6
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-09 14:45:45 +0300

    [HER-125] Follow-up Container prop, change sizes

--- src/core/layout/navbar/header/index.jsx
+++ src/core/layout/navbar/header/index.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 
 import { Home } from 'core/layout/navbar/links/home'
-import { useStyles } from 'core/layout/navbar/header/style'
-import { NavigateToDetections } from 'core/layout/navbar/links/detections'
 import { NavigateToCameras } from 'core/layout/navbar/links/cameras'
+import { NavigateToDetections } from 'core/layout/navbar/links/detections'
 
-export const Header = () => <Grid item xs={5} sm={5} md={5} lg={5} xl={5} classes={useStyles()}>
+export const Header = () => <Grid item container xs={12} sm={8} md={9} lg={10}>
   <Home />
   <NavigateToDetections />
   <NavigateToCameras />
