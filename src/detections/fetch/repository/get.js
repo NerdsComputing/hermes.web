e2ec44206f4771f84ea9af51ab0813983eb27666
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-09 13:10:04 +0300

    [HER-65] Follow-up Add a separate function

--- src/detections/fetch/repository/get.js
+++ src/detections/fetch/repository/get.js
@@ -4,5 +4,10 @@ import { map } from 'rxjs/operators'
 import { GET_DETECTIONS } from 'detections/fetch/repository/query'
 import { client } from 'startup/client'
 
-export const get = payload => from(client().query({ query: GET_DETECTIONS, variables: { parameter: payload } }))
+const buildQuery = payload => ({
+  query: GET_DETECTIONS,
+  variables: { parameter: payload },
+})
+
+export const get = payload => from(client().query(buildQuery(payload)))
   .pipe(map(result => result.data.detections))
