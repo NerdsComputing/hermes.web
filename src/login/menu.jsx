26545a3ad5d6af0eff8fb5aede08174f598335ec
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-09 14:47:43 +0300

    [HER-125] Follow-up Change div into Box

--- src/login/menu.jsx
+++ src/login/menu.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Box } from '@material-ui/core'
 
 import { useContext } from 'login/context'
 import { Button } from 'login/login-button'
@@ -6,7 +7,7 @@ import { Button } from 'login/login-button'
 export const Menu = () => {
   const { user } = useContext()
 
-  return <div>
+  return <Box px={3}>
     <Button user={user} />
-  </div>
+  </Box>
 }
