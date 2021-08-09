c97944c98b3bb444d6a63e5cd4674f49b9477ad3
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-09 14:45:00 +0300

    [HER-125] Follow-up Add Grid and Box wrappers

--- src/core/layout/navbar/links/home.jsx
+++ src/core/layout/navbar/links/home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Link } from '@material-ui/core'
+import * as Material from '@material-ui/core'
 
-import { useNavigation } from 'core/layout/navbar/links/hooks/use-navigation'
 import { useStyles } from 'core/layout/navbar/links/styles'
+import { useNavigation } from 'core/layout/navbar/links/hooks/use-navigation'
 
-export const Home = () => <Link onClick={useNavigation('/')} variant={'h6'} classes={useStyles()}>
-  Hermes
-</Link>
+export const Home = () => <Material.Grid item>
+  <Material.Box px={2} pl={4}>
+    <Material.Link onClick={useNavigation('/')} variant={'h6'} classes={useStyles()}>
+      Hermes
+    </Material.Link>
+  </Material.Box>
+</Material.Grid>
