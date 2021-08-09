24bee7d9f1b1d0b15bb22acd9a41d47268afaa9c
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-09 23:20:51 +0300

    [HER-123] Add marketing component to home content

--- src/home/content.jsx
+++ src/home/content.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 import { Title } from 'home/title'
 import { Carousel } from 'home/carousel'
 import { Live } from 'home/live'
+import { Marketing } from 'home/marketing'
 
 export const Content = () => <>
   <Title />
   <Carousel />
   <Live />
+  <Marketing />
 </>
