dab126a6e2bb6302afa0ef32aa4c1a59c62e6450
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-09 12:07:01 +0300

    [HER-125] Follow-up Move to a new directory

--- src/core/layout/navbar/mobile/content.jsx
+++ src/core/layout/navbar/mobile/content.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { AppBar, Grid } from '@material-ui/core'
 
-import { Home } from 'core/layout/navbar/navigation/home'
-import { Icon } from 'core/layout/navbar/responsive-drawer/mobile/icon'
+import { Home } from 'core/layout/navbar/links/home'
+import { Icon } from 'core/layout/navbar/mobile/icon'
 
 export const Content = () => <AppBar>
   <Grid container justify={'space-between'}>
