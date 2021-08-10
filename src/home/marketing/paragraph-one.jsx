73ab32352c101d46c7b3c67253e8431008e1c9c4
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-10 11:34:54 +0300

    [HER-123] Follow-up Paragraph for extra text

--- src/home/marketing/paragraph-one.jsx
+++ src/home/marketing/paragraph-one.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
 
-export const ParagraphOne = () => <Typography>
+export const ParagraphOne = () => <Typography paragraph>
   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod incididunt ut labore et dolore magna aliqua.
   Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet.
   Semper risus in hendrerit gravida rutrum quisque non tellus. Tellus id interdum velit laoreet id donec ultrices.
