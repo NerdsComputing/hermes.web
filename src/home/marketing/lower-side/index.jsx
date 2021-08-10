457ef141a293c90501783bedff124a764651a982
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-10 14:17:38 +0300

    [HER-123] Follow-up Rename directory and component

--- src/home/marketing/lower-side/index.jsx
+++ src/home/marketing/lower-side/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Box, Grid } from '@material-ui/core'
 
-import { useStyles } from 'home/marketing/right-side/style'
-import { Paragraph } from 'home/marketing/right-side/paragraph'
+import { useStyles } from 'home/marketing/lower-side/style'
+import { Paragraph } from 'home/marketing/lower-side/paragraph'
 
-export const RightContent = () => <Grid item>
+export const LowerContent = () => <Grid item>
   <Grid container className={useStyles().display}>
     <Box px={3}>
       <img src={'https://via.placeholder.com/150'} alt={'marketing'} className={useStyles().floatImage} />
