3882ba53fff9e1572797517da85bbed678d4dca4
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-10 13:42:29 +0300

    [HER-123] Follow-up Wrapped around grid Item

--- src/home/live/index.jsx
+++ src/home/live/index.jsx
@@ -3,7 +3,9 @@ import { Grid } from '@material-ui/core'
 
 import { NavigationButton } from 'home/live/navigation-button'
 
-export const Live = () => <Grid item container justify={'center'} alignItems={'center'}>
-  <NavigationButton path={'/cameras'}>See Cameras</NavigationButton>
-  <NavigationButton path={'/detections'}>See Detections</NavigationButton>
+export const Live = () => <Grid item>
+  <Grid container justify={'center'} alignItems={'center'}>
+    <NavigationButton path={'/cameras'}>See Cameras</NavigationButton>
+    <NavigationButton path={'/detections'}>See Detections</NavigationButton>
+  </Grid>
 </Grid>
