d6dfe933ce44c2c2dfd2d4e2bd1f7cb808529d87
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-10 12:45:53 +0300

    [HER-123] Follow-up Wrapped carousel with Grid

--- src/home/carousel/index.jsx
+++ src/home/carousel/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
 
 import { Slider } from 'home/slider'
-import { useStyle } from 'home/carousel/style'
 import { Arrow } from 'home/arrows/arrow'
+import { useStyle } from 'home/carousel/style'
 
-export const Carousel = () => <div className={useStyle().carousel}>
+
+export const Carousel = () => <Grid item className={useStyle().carousel}>
   <Arrow direction={'left'} />
   <Slider />
   <Arrow direction={'right'} />
-</div>
+</Grid>
