0c09e7b6ab5e9873fb3230557e1f07cc5e795224
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-10 12:46:47 +0300

    [HER-123] Follow-up Wrapped with Grid item

--- src/home/marketing/right-side/index.jsx
+++ src/home/marketing/right-side/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Box, Grid } from '@material-ui/core'
-import { Paragraph } from 'home/marketing/right-side/paragraph'
-import { useStyles } from 'home/marketing/right-side/style'
 
-export const RightContent = () => <Grid container className={useStyles().display}>
-  <Grid item>
+import { useStyles } from 'home/marketing/right-side/style'
+import { Paragraph } from 'home/marketing/right-side/paragraph'
+
+export const RightContent = () => <Grid item>
+  <Grid container className={useStyles().display}>
     <Box px={3}>
       <img src={'https://via.placeholder.com/150'} alt={'marketing'} className={useStyles().floatImage} />
     </Box>
