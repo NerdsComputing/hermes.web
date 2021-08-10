11bbc3693d59e51855994ff585313a9d85fe3101
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-10 15:40:42 +0300

    [HER-120] Combine chart and table

--- src/detections/index.jsx
+++ src/detections/index.jsx
@@ -1,34 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import { Bar } from 'react-chartjs-2'
-import { Grid } from '@material-ui/core'
+import React from 'react'
 
-import { labels } from 'detections/labels.json'
-import { byMode } from 'detections/by-mode'
+import { Table } from 'detections/table'
+import { Content } from 'detections/content'
 
-const hours = state => Object.entries(state)
-  .map(([key, value]) => ({
-    label: key,
-    data: value,
-    backgroundColor: `#${((1 << 24) * Math.random() | 0).toString(16)}`,
-  }))
-
-const data = (state, label) => ({
-  labels: label,
-  datasets: hours(state),
-
-})
-
-const useData = setState => useEffect(() => {
-  byMode(setState, 'hours')
-}, [setState])
-
-export const Detections = () => {
-  const [state, setState] = useState({})
-  useData(setState)
-
-  return <Grid item>
-    <Bar width={'500px'} height={'500px'}
-         options={{ maintainAspectRatio: false }} data={data(state, labels.days)} />
-  </Grid>
-}
+export const Detections = () => <>
+  <Content />
+  <Table />
+</>
 
