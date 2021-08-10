c153643f635893da395257b56db6875d443f6141
bianca <bia.topircean@gmail.com>
2021-08-10 12:52:40 +0300

    [HER-145] Remove array parameter

--- src/detections/chart/modes.js
+++ src/detections/chart/modes.js
@@ -1,5 +1,5 @@
 export const modes = {
-  hours: (array, entry) => array[Math.floor(new Date(entry.timestamp).getMonth() / 6)].push(entry),
-  months: (array, entry) => array[Math.floor(new Date(entry.timestamp).getMonth() / 3)].push(entry),
-  days: (array, entry) => array[Math.floor(new Date(entry.timestamp).getMonth() / 8)].push(entry),
+  hours: entry => Math.floor(new Date(entry.timestamp).getMonth() / 6),
+  months: entry => Math.floor(new Date(entry.timestamp).getMonth() / 3),
+  days: entry => Math.floor(new Date(entry.timestamp).getMonth() / 8),
 }
