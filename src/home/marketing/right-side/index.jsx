1959aebbcae70c890f81ec2b848c40a5606617c4
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-10 11:33:38 +0300

    [HER-123] Follow-up Change directory

--- src/home/marketing/right-side/index.jsx
+++ src/home/marketing/right-side/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Box, Grid } from '@material-ui/core'
-import { Paragraph } from 'home/marketing/paragraph'
-import { useStyles } from 'home/marketing/style'
+import { Paragraph } from 'home/marketing/right-side/paragraph'
+import { useStyles } from 'home/marketing/right-side/style'
 
-export const Content = () => <Grid container className={useStyles().display}>
+export const RightContent = () => <Grid container className={useStyles().display}>
   <Grid item>
     <Box px={3}>
       <img src={'https://via.placeholder.com/150'} alt={'marketing'} className={useStyles().floatImage} />
