9d30eaa05f3bfd3bffb4b14d3fc20eaf8fd199ae
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-10 13:42:45 +0300

    [HER-123] Follow-up Add sizes to Grid

--- src/home/marketing/index.jsx
+++ src/home/marketing/index.jsx
@@ -5,8 +5,10 @@ import { Title } from 'home/marketing/title'
 import { LeftContent } from 'home/marketing/left-side'
 import { RightContent } from 'home/marketing/right-side'
 
-export const Marketing = () => <Grid item container justify={'center'} alignItems={'center'}>
-  <Title />
-  <LeftContent />
-  <RightContent />
+export const Marketing = () => <Grid item xs={10} s={10} md={10} lg={10}>
+  <Grid container justify={'center'} alignItems={'center'}>
+    <Title />
+    <LeftContent />
+    <RightContent />
+  </Grid>
 </Grid>
