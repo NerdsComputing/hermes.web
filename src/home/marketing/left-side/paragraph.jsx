b3e92c09f360770ac5682497d61165ed1361c99a
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-10 11:34:10 +0300

    [HER-123] Follow-up Change directory

--- src/home/marketing/left-side/paragraph.jsx
+++ src/home/marketing/left-side/paragraph.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Box, Grid, Typography } from '@material-ui/core'
 import { ParagraphOne } from 'home/marketing/paragraph-one'
-import { useStyles } from 'home/marketing/style'
+import { useStyles } from 'home/marketing/left-side/style'
 
-export const Paragraph = () => <Grid item xs={12} sm={11} md={11} lg={10}>
-  <Box className={useStyles().box} whiteSpace={'normal'}>
-    <Typography paragraph align={'justify'}>
+export const Paragraph = () => <Grid item xs={12} sm={11} md={11} lg={10} className={useStyles().width}>
+  <Box className={useStyles().box} whiteSpace={'normal'} textAlign={'center'}>
+    <Typography paragraph>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore emagna aliqua.
     </Typography>
     <ParagraphOne />
