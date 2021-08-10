4090344ad96fc41b805395a1ae91a0eec37e916d
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-10 11:34:27 +0300

    [HER-123] Follow-up Add right-side paragraph

---
+++ src/home/marketing/right-side/paragraph.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box, Grid, Typography } from '@material-ui/core'
+import { ParagraphOne } from 'home/marketing/paragraph-one'
+import { useStyles } from 'home/marketing/right-side/style'
+
+export const Paragraph = () => <Grid item xs={12} sm={11} md={11} lg={10} className={useStyles().width}>
+  <Box className={useStyles().box} whiteSpace={'normal'} textAlign={'center'}>
+    <Typography paragraph>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore emagna aliqua.
+    </Typography>
+    <ParagraphOne />
+  </Box>
+</Grid>
