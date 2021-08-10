cd0f4ed9fc0e8124025a886f2466111c745aa180
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-10 11:35:16 +0300

    [HER-123] Follow-up Right side style

---
+++ src/home/marketing/right-side/style.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core'
+
+const floatImage = { float: 'right', paddingLeft: '15px', paddingBottom: '10px' }
+
+const display = { display: 'block' }
+
+const box = { marginLeft: '10px', paddingRight: '10px' }
+
+const width = { maxWidth: '100%' }
+
+export const useStyles = makeStyles({ floatImage, display, box, width })
