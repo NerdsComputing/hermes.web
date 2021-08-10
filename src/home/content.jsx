13380594c9a11c09669683ae10dd485c3484ac4c
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-10 13:42:16 +0300

    [HER-123] Follow-up Remove style from Grid

--- src/home/content.jsx
+++ src/home/content.jsx
@@ -3,11 +3,10 @@ import { Grid } from '@material-ui/core'
 
 import { Live } from 'home/live'
 import { Title } from 'home/title'
-import { useStyles } from 'home/style'
 import { Carousel } from 'home/carousel'
 import { Marketing } from 'home/marketing'
 
-export const Content = () => <Grid container alignItems={'center'} justify={'center'} className={useStyles().align}>
+export const Content = () => <Grid container alignItems={'center'} justify={'center'}>
   <Title />
   <Carousel />
   <Live />
