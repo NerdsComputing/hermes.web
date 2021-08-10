55d233aff906f27fcc0d7d0d6a3eb3339b7b01b0
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-10 14:18:09 +0300

    [HER-123] Follow-up Update components and imports

--- src/home/marketing/index.jsx
+++ src/home/marketing/index.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 
 import { Title } from 'home/marketing/title'
-import { LeftContent } from 'home/marketing/left-side'
-import { RightContent } from 'home/marketing/right-side'
+import { UpperContent } from 'home/marketing/upper-side'
+import { LowerContent } from 'home/marketing/lower-side'
 
 export const Marketing = () => <Grid item xs={10} s={10} md={10} lg={10}>
   <Grid container justify={'center'} alignItems={'center'}>
     <Title />
-    <LeftContent />
-    <RightContent />
+    <UpperContent />
+    <LowerContent />
   </Grid>
 </Grid>
