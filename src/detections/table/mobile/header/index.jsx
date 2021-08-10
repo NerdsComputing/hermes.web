a636eb1cfa3e40879029b54e75f3f7add8d18cfd
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-10 15:32:22 +0300

    [HER-120] Create table`s header mobile version

---
+++ src/detections/table/mobile/header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TableHead, TableRow } from '@material-ui/core'
+
+import { Cells } from 'detections/table/mobile/header/cells'
+
+export const Header = () => <TableHead>
+  <TableRow>
+    <Cells />
+  </TableRow>
+</TableHead>
