1cd32885ce6bf72b6fb4a27b50408dca1d58c1d5
bianca <bia.topircean@gmail.com>
2021-08-10 12:47:30 +0300

    [HER-145] Use groupBy for classify detections by class

--- src/detections/chart/by-mode.js
+++ src/detections/chart/by-mode.js
@@ -1,36 +1,7 @@
-import { from, reduce } from 'rxjs'
-import { mergeMap } from 'rxjs/operators'
+import { groupBy } from 'lodash'
 
-import { modes } from 'detections/chart/modes'
+import { countByMode } from 'detections/chart/count-by-mode'
+import { byClass } from 'detections/chart/by-class'
 
-const countDetections = (value, mode) => {
-  const array = [[], [], [], []]
-
-  value.map(entry => modes[mode](array, entry))
-
-  return array.map(entry => entry.length)
-}
-
-const addToClass = (acc, current) => {
-  acc[current.class].push(current)
-
-  return acc
-}
-
-const addClass = (acc, current) => {
-  acc[current.class] = [current]
-
-  return acc
-}
-
-const accumulator = mode => (acc, [key, value]) => {
-  acc[key] = countDetections(value, mode)
-
-  return acc
-}
-
-export const byMode = ({ setState, mode, detections }) => from(detections)
-  .pipe(reduce((acc, current) => acc[current.class] ? addToClass(acc, current) : addClass(acc, current), {}))
-  .pipe(mergeMap(data => Object.entries(data)))
-  .pipe(reduce(accumulator(mode), {}))
-  .subscribe(acc => setState(acc))
+export const byMode = (items, mode) => Object.entries(groupBy(items, byClass))
+  .map(([itemClass, detections]) => [itemClass, countByMode(detections, mode)])
