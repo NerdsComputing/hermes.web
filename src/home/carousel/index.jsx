e5e7045ca934a73ffb8082bad444f953d1f0f66a
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-10 12:48:31 +0300

    [HER-123] Follow-up Remove blank lines

--- src/home/carousel/index.jsx
+++ src/home/carousel/index.jsx
@@ -5,7 +5,6 @@ import { Slider } from 'home/slider'
 import { Arrow } from 'home/arrows/arrow'
 import { useStyle } from 'home/carousel/style'
 
-
 export const Carousel = () => <Grid item className={useStyle().carousel}>
   <Arrow direction={'left'} />
   <Slider />
