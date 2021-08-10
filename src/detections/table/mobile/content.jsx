a23603b83edb403415f1f30c3f75f75bb142afc2
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-10 15:38:47 +0300

    [HER-120] Create the table content in mobile view

---
+++ src/detections/table/mobile/content.jsx
@@ -0,0 +1,13 @@
+import { Table, TableContainer } from '@material-ui/core'
+import React from 'react'
+
+import { useTableStyle } from 'detections/table/hooks/use-table-style'
+import { Header } from 'detections/table/mobile/header'
+import { TableContent } from 'detections/table/mobile/body'
+
+export const Content = () => <TableContainer classes={useTableStyle()}>
+  <Table stickyHeader>
+    <Header />
+    <TableContent />
+  </Table>
+</TableContainer>
