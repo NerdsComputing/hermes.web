aa06f2624c28aaf789a0b79b74efbbfbfe5cb555
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-11 12:24:33 +0300

    [HER-120] Follow-up Remove grid component from mobile version

--- src/detections/table/mobile/index.jsx
+++ src/detections/table/mobile/index.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { Grid, Hidden } from '@material-ui/core'
+import { Hidden } from '@material-ui/core'
 
 import { Content } from 'detections/table/mobile/content'
 import { Pagination } from 'detections/table/pagination'
 
-export const MobileVersion = () => <Grid item>
-  <Grid container>
-    <Hidden smUp>
-      <Content />
-      <Pagination />
-    </Hidden>
-  </Grid>
-</Grid>
+export const MobileVersion = () => <Hidden smUp>
+  <Content />
+  <Pagination />
+</Hidden>
