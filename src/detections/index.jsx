6b588af3f376452c37c4d0d2f1fe07235d56ab4d
bianca <bia.topircean@gmail.com>
2021-08-11 14:01:05 +0300

    [HER-145] Use behaviour in detections

--- src/detections/index.jsx
+++ src/detections/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
 import { Chart } from 'detections/chart'
+import { Behaviour } from 'detections/behaviour'
 
-export const Detections = () => <Chart />
+export const Detections = () => <>
+  <Behaviour />
+  <Chart />
+</>
