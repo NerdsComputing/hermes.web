fffecef9c8048fa4a54e27dd0646c7502ddb47b2
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-11 15:09:26 +0300

    [HER-120] Follow-up Remake the index file

--- src/detections/table/index.jsx
+++ src/detections/table/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 
-import { DesktopVersion } from 'detections/table/desktop'
-import { MobileVersion } from 'detections/table/mobile'
+import { Pagination } from 'detections/table/pagination'
+import { Content } from 'detections/table/content'
 
 export const Table = () => <Grid item xs={12} sm={12} md={12} lg={12}>
-  <DesktopVersion />
-  <MobileVersion />
+  <Content />
+  <Pagination />
 </Grid>
