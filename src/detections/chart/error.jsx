a453bc0d5f92996bfcd7549524a59f41a0322d26
bianca <bia.topircean@gmail.com>
2021-08-11 13:59:11 +0300

    [HER-145] Create component for error message

---
+++ src/detections/chart/error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import HighlightOffIcon from '@material-ui/icons/HighlightOff'
+import { Typography } from '@material-ui/core'
+import { CenterMessage } from 'detections/chart/center-message'
+
+export const Error = () => <CenterMessage>
+  <HighlightOffIcon style={{ fontSize: 70 }} />
+  <Typography>Sorry, something went wrong</Typography>
+</CenterMessage>
