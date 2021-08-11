c5ce07e266f2cb322341574ee3add838b37320b8
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-11 11:39:58 +0300

    [HER-120] Follow-up Add onClick on rows for mobile

--- src/detections/table/mobile/body/row.jsx
+++ src/detections/table/mobile/body/row.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { TableCell, TableRow } from '@material-ui/core'
 
-export const Row = item => <TableRow>
-  <TableCell align={'center'}>item.class</TableCell>
-  <TableCell align={'center'}>item.score</TableCell>
+import { useNavigation } from 'detections/table/hooks/use-navigation'
+import { useRowsStyle } from 'detections/table/hooks/use-rows-style'
+
+export const Row = ({ item }) => <TableRow onClick={useNavigation(`/detection/${item.id}`)} classes={useRowsStyle()}>
+  <TableCell align={'center'}>{item.class}</TableCell>
+  <TableCell align={'center'}>{item.score}</TableCell>
 </TableRow>
+
+Row.propTypes = { item: PropTypes.object.isRequired }
