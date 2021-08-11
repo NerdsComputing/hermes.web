cfaeb5fc3215c867ec27fa669f7976d9cf2a8c95
Casian Urzică-Platon <72656598+Urzica-Platon-Casian@users.noreply.github.com>
2021-08-11 11:54:19 +0300

    [HER-124] Add meet our team button (#35)
    * [HER-124] Create MeetTeam component
    
    * [HER-124] Create navigation button component
    
    * [HER-124] Create navigation hook
    
    * [HER-124] Add MeetTeam component to Home content
    
    * [HER-124] Add MeetTeam component under Marketing

--- src/home/content.jsx
+++ src/home/content.jsx
@@ -5,10 +5,12 @@ import { Live } from 'home/live'
 import { Title } from 'home/title'
 import { Carousel } from 'home/carousel'
 import { Marketing } from 'home/marketing'
+import { MeetTeam } from 'home/meet-team'
 
 export const Content = () => <Grid container alignItems={'center'} justify={'center'}>
   <Title />
   <Carousel />
   <Live />
   <Marketing />
+  <MeetTeam />
 </Grid>
---
+++ src/home/meet-team/navigation-button.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Box, Button, Grid } from '@material-ui/core'
+
+import { useNavigation } from 'home/meet-team/hooks/use-navigation'
+
+export const NavigationButton = ({ children, path }) => <Grid item>
+  <Box px={5} py={1}>
+    <Button variant={'contained'} color={'primary'} onClick={useNavigation(path)}>
+      {children}
+    </Button>
+  </Box>
+</Grid>
+
+NavigationButton.propTypes = {
+  children: PropTypes.array.isRequired,
+  path: PropTypes.string.isRequired,
+}
