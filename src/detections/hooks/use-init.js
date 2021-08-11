c4d99b848622bc326e61c334a2938f4f605df03c
bianca <bia.topircean@gmail.com>
2021-08-11 13:58:35 +0300

    [HER-145] Reorder imports

--- src/detections/hooks/use-init.js
+++ src/detections/hooks/use-init.js
@@ -1,10 +1,12 @@
-import { useDispatch } from 'react-redux'
-import { actions } from 'detections/fetch/slices/get'
 import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { actions } from 'detections/fetch/slices/get'
 
 export const useInit = () => {
   const dispatch = useDispatch()
   const { attempt } = actions
+
   useEffect(() => {
     dispatch(attempt({ pagination: { pageIndex: 0, pageSize: 1000 } }))
   }, [dispatch, attempt])
