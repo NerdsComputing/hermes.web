5f014376d749ca3eb5ff555a801e07b9ebfcb357
bianca <bia.topircean@gmail.com>
2021-08-11 14:01:39 +0300

    [HER-145] Remove console.log from hook

--- src/detections/chart/use-items.js
+++ src/detections/chart/use-items.js
@@ -1,8 +1,4 @@
 import { useSelector } from 'react-redux'
 import { get } from 'lodash'
 
-export const useItems = () => {
-  console.log(useSelector(({ detections }) => get(detections, 'get.data.items', [])))
-
-  return useSelector(({ detections }) => get(detections, 'get.data.items', []))
-}
+export const useItems = () => useSelector(({ detections }) => get(detections, 'get.data.items', []))
