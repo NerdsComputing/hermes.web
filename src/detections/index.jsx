ebbf2f7766bcfa644f164790a8fc590bbcd1d555
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-11 12:22:56 +0300

    [HER-120] Follow-up Add grid container on detections page

--- src/detections/index.jsx
+++ src/detections/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
 
 import { Table } from 'detections/table'
 import { Content } from 'detections/content'
 
-export const Detections = () => <>
+export const Detections = () => <Grid container>
   <Content />
   <Table />
-</>
+</Grid>
 
