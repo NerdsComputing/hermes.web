e0cafd3df1da8991706bbe892d57596bd8d8e7b8
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-11 10:18:52 +0300

    [HER-123] Follow-up Add Content to index

--- src/home/marketing/lower-side/index.jsx
+++ src/home/marketing/lower-side/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box, Grid } from '@material-ui/core'
 
 import { useStyles } from 'home/marketing/lower-side/style'
-import { Paragraph } from 'home/marketing/lower-side/paragraph'
+import { Content } from 'home/marketing/lower-side/content'
 
 export const LowerContent = () => <Grid item>
   <Grid container className={useStyles().display}>
@@ -10,5 +10,5 @@ export const LowerContent = () => <Grid item>
       <img src={'https://via.placeholder.com/150'} alt={'marketing'} className={useStyles().floatImage} />
     </Box>
   </Grid>
-  <Paragraph />
+  <Content />
 </Grid>
