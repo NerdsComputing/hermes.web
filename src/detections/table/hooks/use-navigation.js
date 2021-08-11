a1295162827722a5af6633bd80084c35ad4c688e
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-11 11:39:39 +0300

    [HER-120] Follow-up Create hook for onClick action

---
+++ src/detections/table/hooks/use-navigation.js
@@ -0,0 +1,10 @@
+import { useHistory } from 'react-router-dom'
+import { useCallback } from 'react'
+
+export const useNavigation = path => {
+  const history = useHistory()
+
+  return useCallback(() => {
+    history.push(path)
+  }, [history, path])
+}
