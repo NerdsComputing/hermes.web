1ee598644989ab53c382cb66683ec83ce775d1d0
Codrut <constantinescu321@gmail.com>
2021-08-12 19:31:31 +0300

    [HER-117] Follow-up Remove properties

--- src/detections/filters/accordion-details/input-wrapper.jsx
+++ src/detections/filters/accordion-details/input-wrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 import PropTypes from 'prop-types'
 
-export const InputWrapper = ({ children }) => <Grid container item xs={11} sm={7} md={8} alignItems={'center'}>
+export const InputWrapper = ({ children }) => <Grid item xs={11} sm={8} md={9} lg={9} xl={9}>
   {children}
 </Grid>
 
