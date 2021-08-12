c7366e1c0b7ab9c0834e7cefd8a93f7455c0a10f
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-12 11:34:37 +0300

    [HER-120] Follow-up Delete unused files

--- src/detections/table/body/rows.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-import { detections } from 'detections/table/body/data'
-import { Row } from 'detections/table/body/row'
-
-export const Rows = () => detections.map((item, index) => <Row item={item} key={index} />)
