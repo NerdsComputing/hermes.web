cf269013736094c1851c446e7712ce5485547d95
bianca <bia.topircean@gmail.com>
2021-08-12 16:06:32 +0300

    [HER-122] Add minHeight for page

--- src/page-not-found/style.js
+++ src/page-not-found/style.js
@@ -1,3 +1,6 @@
 import { makeStyles } from '@material-ui/core'
 
-export const useStyles = makeStyles(() => ({ root: { fontSize: '30px' } }))
+export const useStyles = makeStyles(() => ({
+  root: { minHeight: '883px' },
+  textSize: { fontSize: '30px' },
+}))
