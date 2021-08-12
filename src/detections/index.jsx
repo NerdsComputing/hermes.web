3037306bb26a75c516916fbeb7366ed4e086e55c
Codrut <constantinescu321@gmail.com>
2021-08-12 19:46:00 +0300

    [HER-117] Follow-up Add Filters

--- src/detections/index.jsx
+++ src/detections/index.jsx
@@ -2,17 +2,14 @@ import React from 'react'
 
 import { Wrapper } from 'detections/wrapper'
 import { Filters } from 'detections/filters'
-import { Bar } from 'detections/bar'
 import { Table } from 'detections/table'
-
-export const Detections = () => <>
-  <Table />
+import { Chart } from 'detections/chart'
+import { Behaviour } from 'detections/behaviour'
 
 export const Detections = () => <Wrapper>
-    <Behaviour />
-    <Filters/>
-    <Chart />      
-    <Bar/>
-  </Wrapper>
-}
+  <Behaviour />
+  <Filters />
+  <Chart />
+  <Table />
+</Wrapper>
 
