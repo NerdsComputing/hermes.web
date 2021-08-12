231f66f5dcbbf6dcb167c3a745968b20446624ae
bianca <bia.topircean@gmail.com>
2021-08-12 16:07:09 +0300

    [HER-122] Use useProps hook

--- src/page-not-found/index.jsx
+++ src/page-not-found/index.jsx
@@ -3,8 +3,9 @@ import { Grid, Typography } from '@material-ui/core'
 
 import pig from 'page-not-found/pig.jpg'
 import { useStyles } from 'page-not-found/style'
+import { useProps } from 'page-not-found/use-props'
 
-export const PageNotFound = () => <Grid container justify={'center'} alignItems={'center'} direction={'column'}>
+export const PageNotFound = () => <Grid {...useProps()}>
   <img src={pig} />
-  <Typography variant={'overline'} classes={useStyles()}>Oh, no! Page not found.</Typography>
+  <Typography variant={'overline'} className={useStyles().textSize}>Oh, no! Page not found.</Typography>
 </Grid>
