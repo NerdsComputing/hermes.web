f49ee30c794b741508c83415d458fb9fb67582e6
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-12 12:12:05 +0300

    [HER-120] Follow-up Make useCellProps a complex object

--- src/detections/table/body/row.jsx
+++ src/detections/table/body/row.jsx
@@ -4,14 +4,14 @@ import { TableCell, TableRow } from '@material-ui/core'
 
 import { useNavigation } from 'detections/table/hooks/use-navigation'
 import { useRowsStyle } from 'detections/table/hooks/use-rows-style'
-import { useRowProps } from 'detections/table/body/hooks/use-row-props'
+import { useCellProps } from 'detections/table/body/hooks/use-cell-props'
 
 export const Row = ({ item }) => <TableRow onClick={useNavigation(`/detection/${item.id}`)} classes={useRowsStyle()}>
-  <TableCell {...useRowProps(true)}>{item.id}</TableCell>
-  <TableCell {...useRowProps(false)}>{item.class}</TableCell>
-  <TableCell {...useRowProps(true)}>{item.score}</TableCell>
-  <TableCell {...useRowProps(false)}>{item.timeStamp}</TableCell>
-  <TableCell {...useRowProps(true)}>cameraid</TableCell>
+  <TableCell {...useCellProps({ hideOnMobile: true })}>{item.id}</TableCell>
+  <TableCell {...useCellProps({ hideOnMobile: false })}>{item.class}</TableCell>
+  <TableCell {...useCellProps({ hideOnMobile: true })}>{item.score}</TableCell>
+  <TableCell {...useCellProps({ hideOnMobile: false })}>{item.timeStamp}</TableCell>
+  <TableCell {...useCellProps({ hideOnMobile: true })}>cameraid</TableCell>
 </TableRow>
 
 Row.propTypes = { item: PropTypes.object.isRequired }
