f8eac848e4bf3f440bbe2e2d246aa32c9634ed5b
bianca <bia.topircean@gmail.com>
2021-08-12 13:17:02 +0300

    [HER-145] Resolve conflicts

--- src/detections/index.jsx
+++ src/detections/index.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 import { Chart } from 'detections/chart'
 import { Behaviour } from 'detections/behaviour'
+import { Table } from 'detections/table'
 
 export const Detections = () => <>
   <Behaviour />
   <Chart />
+  <Table />
 </>
