12370534e2c2ef1501b073a4c0060557f1a585d4
bianca <bia.topircean@gmail.com>
2021-08-12 13:47:16 +0300

    [HER-145] Follow-up Format hook

--- src/detections/chart/hooks/use-message-props.js
+++ src/detections/chart/hooks/use-message-props.js
@@ -1,8 +1,7 @@
 import { useStyles } from 'detections/chart/style'
 
-export const useMessageProps = () => (
-  {
-    container: 'true',
+export const useMessageProps = () => ({
+    container: true,
     justify: 'center',
     direction: 'column',
     alignItems: 'center',
