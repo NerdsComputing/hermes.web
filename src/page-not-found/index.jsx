bd3e0d394bf2acee6c9571ff839348b373149789
bianca <bia.topircean@gmail.com>
2021-08-12 14:51:23 +0300

    [HER-122] Add text

--- src/page-not-found/index.jsx
+++ src/page-not-found/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Box, Grid } from '@material-ui/core'
+import { Grid, Typography } from '@material-ui/core'
+
 import pig from 'page-not-found/pig.jpg'
+import { useStyles } from 'page-not-found/style'
 
-export const PageNotFound = () => <Grid container justify={'center'}>
-  <Box display={'flex'} justifyContent={'center'}>
-    <img src={pig} />
-  </Box>
+export const PageNotFound = () => <Grid container justify={'center'} alignItems={'center'} direction={'column'}>
+  <img src={pig} />
+  <Typography variant={'overline'} classes={useStyles()}>Oh, no! Page not found.</Typography>
 </Grid>
-
