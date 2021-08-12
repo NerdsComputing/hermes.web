b2a9b4a6af47cdac8c1cd75ed589d0736702f930
Codrut <constantinescu321@gmail.com>
2021-08-12 19:32:06 +0300

    [HER-117] Follow-up Remove properties

--- src/detections/filters/accordion-details/filter-title.jsx
+++ src/detections/filters/accordion-details/filter-title.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { Grid, Typography } from '@material-ui/core'
 import PropTypes from 'prop-types'
 
-export const FilterTitle = ({ title }) => <Grid container item xs={12} sm={3} md={2} ls={2} xl={2}
-                                                alignContent={'center'}>
+export const FilterTitle = ({ title }) => <Grid item xs={12} sm={3} md={2} ls={2} xl={2}>
   <Typography variant={'h6'}>{title}</Typography>
 </Grid>
 
