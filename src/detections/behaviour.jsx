98a98fc72253f05f12696c22aa82273c0b61861a
Codrut <constantinescu321@gmail.com>
2021-08-12 19:20:52 +0300

    [HER-117] Follow-up Create behaviour component

--- src/detections/behaviour.jsx
+++ src/detections/behaviour.jsx
@@ -1,7 +1,9 @@
 import { useGetDetections } from 'detections/hooks/use-get-detections'
+import { useFetchCameras } from 'detections/hooks/use-fetch-cameras'
 
 export const Behaviour = () => {
   useGetDetections()
+  useFetchCameras()
 
   return ''
 }
