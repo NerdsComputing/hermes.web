18e01bbf919801d75f8055707b7d06f9ae854ba5
bianca <bia.topircean@gmail.com>
2021-08-12 16:06:11 +0300

    [HER-122] Add redirect to 404

--- src/startup/mapping.jsx
+++ src/startup/mapping.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Redirect, Route } from 'react-router-dom'
 
 import { routes } from 'startup/routes'
 import { Layout } from 'core/layout/layout'
+import { PageNotFound } from 'page-not-found'
 
 export const Mapping = () => <Layout>
   {routes.map(route => <Route exact path={route.path} key={route.path}>
     <route.component />
   </Route>)}
+  <Route path={'/404'} component={PageNotFound} />
+  <Redirect to={'/404'} />
 </Layout>
-
