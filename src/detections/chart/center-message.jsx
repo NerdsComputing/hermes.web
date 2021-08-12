37fc3e9a793b06515defcb1010300932e872ba21
bianca <bia.topircean@gmail.com>
2021-08-12 11:02:36 +0300

    [HER-145] Follow-up Reorder imports

--- src/detections/chart/center-message.jsx
+++ src/detections/chart/center-message.jsx
@@ -1,7 +1,8 @@
-import { useStyles } from 'detections/chart/style'
 import { Grid } from '@material-ui/core'
 import React from 'react'
 import PropTypes from 'prop-types'
+
+import { useStyles } from 'detections/chart/style'
 
 export const CenterMessage = ({ children }) => <Grid container justify={'center'} direction={'column'}
                                                      alignItems={'center'} classes={useStyles()}>
