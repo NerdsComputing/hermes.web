0cb6e2977855d6b990a8c89c889babfe0749f849
Codrut <constantinescu321@gmail.com>
2021-08-13 11:28:00 +0300

    [HER-117] Follow-up Add bottom margin

--- src/detections/filters/desktop-style.js
+++ src/detections/filters/desktop-style.js
@@ -1,5 +1,5 @@
 import { makeStyles } from '@material-ui/core'
 
-const root = { width: '80%' }
+const root = { width: '80%', marginBottom: 50 }
 
 export const desktopStyles = makeStyles({ root })
--- src/detections/filters/mobile-style.js
+++ src/detections/filters/mobile-style.js
@@ -1,5 +1,5 @@
 import { makeStyles } from '@material-ui/core'
 
-const root = { width: '100%' }
+const root = { width: '100%', marginBottom: 50 }
 
 export const mobileStyles = makeStyles(({ root }))
