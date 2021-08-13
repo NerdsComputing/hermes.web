7a14270e4ca32373d00a7a28968814fc4f13f88f
Codrut <constantinescu321@gmail.com>
2021-08-13 11:30:19 +0300

    [HER-117] Follow-up Create content component

--- src/detections/filters/accordion-details/score/index.jsx
+++ src/detections/filters/accordion-details/score/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import { Input } from 'detections/filters/accordion-details/score/input'
 import { RemoveButton } from 'detections/filters/accordion-details/remove-button'
 import { FilterWrapper } from 'detections/filters/accordion-details/filter-wrapper'
 import { FilterTitle } from 'detections/filters/accordion-details/filter-title'
+import { Content } from 'detections/filters/accordion-details/score/content'
 
 export const Score = index => <FilterWrapper>
   <FilterTitle title={'Score'} />
-  <Input />
+  <Content />
   <RemoveButton index={index} type={'Score'} />
 </FilterWrapper>
