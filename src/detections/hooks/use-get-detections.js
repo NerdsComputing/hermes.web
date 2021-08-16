09fac1b100fd7c9264fe2a6b71e84ed4c6ac4a7e
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-16 12:35:10 +0300

    [HER-146] Use the pagination component for dispatch

--- src/detections/hooks/use-get-detections.js
+++ src/detections/hooks/use-get-detections.js
@@ -2,12 +2,14 @@ import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
 import { actions } from 'detections/fetch/slices/get'
+import { useContext } from 'detections/context'
 
 export const useGetDetections = () => {
   const dispatch = useDispatch()
   const { attempt } = actions
+  const { pageIndex, pageSize } = useContext()
 
   useEffect(() => {
-    dispatch(attempt({ pagination: { pageIndex: 0, pageSize: 1000 } }))
-  }, [dispatch, attempt])
+    dispatch(attempt({ pagination: { pageIndex, pageSize } }))
+  }, [dispatch, attempt, pageIndex, pageSize])
 }
