b71eab2b06a2cbc102d05d77b3f970dbcb5d577f
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-16 12:32:08 +0300

    [HER-146] The implementation of the pagination

--- src/detections/table/pagination/index.jsx
+++ src/detections/table/pagination/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
 
 import { TablePagination } from '@material-ui/core'
+import { useChangePage } from 'detections/table/pagination/hooks/use-change-page'
+import { useChangeRowsPerPage } from 'detections/table/pagination/hooks/use-change-rows-per-page'
+import { useContext } from 'detections/context'
+import { useSelector } from 'react-redux'
+import { get } from 'lodash'
 
-export const Pagination = () => <TablePagination component={'div'} />
+export const Pagination = () => {
+  const { pageIndex, pageSize } = useContext()
+  const count = useSelector(({ detections }) => get(detections, 'get.data.totalCount', []))
+
+  return <TablePagination component={'div'} count={count} page={pageIndex}
+                          rowsPerPage={pageSize}
+                          onChangeRowsPerPage={useChangeRowsPerPage()}
+                          onChangePage={useChangePage()} />
+}
