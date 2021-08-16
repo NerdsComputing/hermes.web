3b7d5d91bf0d9ee4390871c5265d8605a5f979a7
bianca <bia.topircean@gmail.com>
2021-08-16 15:10:56 +0300

    [HER-135] Follow-up Add a Grid item wrapper

--- src/cameras/content.jsx
+++ src/cameras/content.jsx
@@ -5,6 +5,8 @@ import YouTube from 'react-youtube'
 import { settings } from 'settings'
 import { useStyles } from 'cameras/style'
 
-export const Content = () => <Grid container xs={12} md={9} justify={'center'} classes={useStyles()}>
-  <YouTube videoId={settings().sources[0]} />
+export const Content = () => <Grid item xs={12} md={9}>
+  <Grid container justify={'center'} classes={useStyles()}>
+    <YouTube videoId={settings().sources[0]} />
+  </Grid>
 </Grid>
