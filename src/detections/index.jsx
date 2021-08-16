5b032ae4ca19533e8a88d43bac6e6bad03180d21
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-16 12:30:19 +0300

    [HER-146] Move context in index file

--- src/detections/index.jsx
+++ src/detections/index.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 import { Wrapper } from 'detections/wrapper'
 import { Filters } from 'detections/filters'
-import { Table } from 'detections/table'
 import { Chart } from 'detections/chart'
 import { Behaviour } from 'detections/behaviour'
+import { Table } from 'detections/table'
+import { useDefaultContext, Context } from 'detections/context'
 
 export const Detections = () => <Wrapper>
-  <Behaviour />
-  <Filters />
-  <Chart />
-  <Table />
+  <Context.Provider value={useDefaultContext()}>
+    <Behaviour />
+    <Filters />
+    <Chart />
+    <Table />
+  </Context.Provider>
 </Wrapper>
-
