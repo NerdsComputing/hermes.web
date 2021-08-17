4db84098fc80d80633700567cb096ea0ae85484d
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-17 17:14:58 +0300

    [HER-128] Update useInputProps hook

--- src/cameras/search/hooks/use-input-props.js
+++ src/cameras/search/hooks/use-input-props.js
@@ -1,22 +1,9 @@
 import { useSearchbarStyle } from 'cameras/search/hooks/use-searchbar-style'
-import { useContext } from 'cameras/context'
-import { useRequestSearch } from 'cameras/search/hooks/use-request-search'
-import { useCancelSearch } from 'cameras/search/hooks/use-cancel-search'
 
-const useCallbacks = () => {
-  const onChange = useRequestSearch()
-  const onCancelSearch = useCancelSearch()
-
-  return ({
-    onChange,
-    onCancelSearch,
-  })
-}
-
-export const useInputProps = () => (
-  {
-    value: useContext().searchPhrase,
-    placeholder: 'Cameras by id',
-    ...useCallbacks(),
-    classes: useSearchbarStyle(),
-  })
+export const useInputProps = setSearchInput => ({
+  onChange: value => {
+    setSearchInput(value)
+  },
+  placeholder: 'Cameras by id',
+  classes: useSearchbarStyle(),
+})
