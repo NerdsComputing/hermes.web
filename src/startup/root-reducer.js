344ea7bf6037971f1f438e65c61e20df8f645dce
bianca <bia.topircean@gmail.com>
2021-08-17 17:08:59 +0300

    [HER-152] Add detection to root

--- src/startup/root-reducer.js
+++ src/startup/root-reducer.js
@@ -2,5 +2,6 @@ import { combineReducers } from '@reduxjs/toolkit'
 
 import { detections } from 'detections/fetch/slices'
 import { cameras } from 'cameras/slices'
+import { detection } from 'detection/fetch/slices'
 
-export const rootReducer = combineReducers({ detections, cameras })
+export const rootReducer = combineReducers({ detections, cameras, detection })
