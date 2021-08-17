d5a79989d4980e26a173f45263aed3277bb12519
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-17 17:13:31 +0300

    [HER-128] Add spacing to imports

--- src/cameras/sidebar.jsx
+++ src/cameras/sidebar.jsx
@@ -1,7 +1,8 @@
-import * as Material from '@material-ui/core'
-import { Header } from 'cameras/header'
-import { Ids } from 'cameras/list'
 import React from 'react'
+import * as Material from '@material-ui/core'
+
+import { Ids } from 'cameras/list'
+import { Header } from 'cameras/header'
 
 export const Sidebar = () => <Material.Grid item xs={12} md={3}>
   <Header />
