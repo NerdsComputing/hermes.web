b3a9ebaec9f0280983f554b979b43c9ec34966e1
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-17 17:12:46 +0300

    [HER-128] Change search input component

--- src/cameras/search/search-input.jsx
+++ src/cameras/search/search-input.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
 import SearchBar from 'material-ui-search-bar'
 
+import { SearchButton } from 'cameras/search/search-button'
 import { useInputProps } from 'cameras/search/hooks/use-input-props'
+import { useContext } from 'cameras/context'
 
-export const SearchInput = () => <SearchBar {...useInputProps()} />
+export const SearchInput = () => <Grid item xs={12} sm={12} md={12} lg={12}>
+  <SearchBar {...useInputProps(useContext().setSearchInput)} />
+  <Grid container alignItems={'center'} justify={'center'}>
+    <SearchButton value={useContext().searchInput}>Search Cameras</SearchButton>
+  </Grid>
+</Grid>
 
