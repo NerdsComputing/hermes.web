a8eb4112c8efa1e5692c4568c632b32c1a8366dc
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-17 17:08:28 +0300

    [HER-128] Add map to content

--- src/cameras/content.jsx
+++ src/cameras/content.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
 import YouTube from 'react-youtube'
 import * as Material from '@material-ui/core'
 
+import { Map } from 'cameras/map'
 import { settings } from 'settings'
 import { useStyles } from 'cameras/style'
 
-export const Content = () => <Grid item xs={12} md={9}>
-  <Grid container justify={'center'} classes={useStyles()}>
-    <YouTube videoId={settings().sources[0]} />
-  </Grid>
-</Grid>
-import { Map } from 'cameras/map'
-
-export const Content = () => <Material.Grid item xs={12} sm={11} md={8} lg={8}>
+export const Content = () => <Material.Grid item xs={12} sm={11} md={9} lg={8}>
   <Material.Box pt={3} style={{ height: '80vh', width: '100%' }}>
     <Map />
   </Material.Box>
+  <Material.Grid container justify={'center'} classes={useStyles()}>
+    <YouTube videoId={settings().sources[0]} />
+  </Material.Grid>
 </Material.Grid>
+
