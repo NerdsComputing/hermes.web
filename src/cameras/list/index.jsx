91054a467e59456bd288adcb6b1fe52be6b2078c
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-17 17:10:17 +0300

    [HER-128] Add Behaviour component to Ids

--- src/cameras/list/index.jsx
+++ src/cameras/list/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { List } from '@material-ui/core'
 
+import { Search } from 'cameras/search'
 import { Items } from 'cameras/list/items'
 import { useListStyle } from 'cameras/list/hooks/use-list-style'
-import { List } from '@material-ui/core'
-import { Search } from 'cameras/search'
+import { Behaviour } from 'cameras/behaviour'
 
 export const Ids = () => <>
+  <Behaviour />
   <Search />
   <List classes={useListStyle()}>
     <Items />
