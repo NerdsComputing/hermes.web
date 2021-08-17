0b7b3747537f8134aed95477ec4ab8c2d951f4b8
bianca <bia.topircean@gmail.com>
2021-08-17 17:05:06 +0300

    [HER-152] Use format timestamp

--- src/detection/card/items/timestamp.jsx
+++ src/detection/card/items/timestamp.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Box, Typography } from '@material-ui/core'
 import { WatchLater } from '@material-ui/icons'
+import PropTypes from 'prop-types'
 
 import { useStyles } from 'detection/card/items/style'
+import { DateFormat } from 'detection/card/items/date-format'
 
-export const CreatedAt = () => <Box classes={useStyles()}>
+export const Timestamp = ({ item }) => <Box classes={useStyles()}>
   <WatchLater />
   <Typography variant={'h6'} align={'center'} className={useStyles().contentText}>
-    Created at
+    {DateFormat(item.timestamp)}
   </Typography>
 </Box>
+
+Timestamp.propTypes = { item: PropTypes.object.isRequired }
+
