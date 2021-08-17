f42072abc5674638cdfc13e83b32e4bb27eef6ee
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-17 17:10:40 +0300

    [HER-128] Mark cameras on map

--- src/cameras/map/index.jsx
+++ src/cameras/map/index.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 import GoogleMapReact from 'google-map-react'
 
-import { Marker } from 'cameras/map/marker'
 import { useSettings } from 'settings/hooks/use-settings'
+import { useStyles } from 'cameras/map/style'
+import { useItems } from 'cameras/list/items/hooks/use-item'
+import { Marker } from 'cameras/map/marker'
 
-export const Map = () => <Grid container style={{ height: '80vh', width: '100%' }}>
+export const Map = () => <Grid container className={useStyles().grid}>
   <GoogleMapReact defaultCenter={{ lat: 45.79, lng: 24.13 }} defaultZoom={15}
                   bootstrapURLKeys={{ key: useSettings().google.map }}>
-    <Marker lat={45.793128} lng={24.131900} text={'1'} />
-    <Marker lat={45.793130} lng={24.132900} text={'2'} />
-    <Marker lat={45.793140} lng={24.129900} text={'3'} />
+    {useItems().map((item, index) => <Marker item={item} key={index} lat={item.latitude} lng={item.longitude} />)}
   </GoogleMapReact>
 </Grid>
