5fa8df3fb0d0550edc7ad0a8d452c6a53901f467
bianca <bia.topircean@gmail.com>
2021-08-18 14:13:02 +0300

    [HER-152] Create subdirectory for score

--- src/detection/card/items/score/index.jsx
+++ src/detection/card/items/score/index.jsx
@@ -3,9 +3,9 @@ import { Box } from '@material-ui/core'
 import CheckIcon from '@material-ui/icons/Check'
 
 import { useStyles } from 'detection/card/items/style'
-import { ScoreItem } from 'detection/card/items/score-item'
+import { Item } from 'detection/card/items/score/item'
 
 export const Score = () => <Box classes={useStyles()}>
   <CheckIcon />
-  <ScoreItem />
+  <Item />
 </Box>
