34f9c2cbb86bf2b566d936d186a6acfa402bb66d
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-18 17:11:32 +0300

    [HER-128] Follow-up Change directory

--- src/cameras/sidebar/header/index.jsx
+++ src/cameras/sidebar/header/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Box, Typography } from '@material-ui/core'
 
-import { useHeaderStyle } from 'cameras/header/hooks/use-header-style'
+import { useHeaderStyle } from 'cameras/sidebar/header/hooks/use-header-style'
 
 export const Header = () => <Box pt={3}>
   <Typography variant={'h6'} classes={useHeaderStyle()}>Cameras</Typography>
