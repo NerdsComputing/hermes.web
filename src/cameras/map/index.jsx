0dfaed7db16157363b3187840ae8fc424ec8ca72
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-18 13:27:37 +0300

    [HER-128] Follow-up Extract map props to hook

--- src/cameras/map/index.jsx
+++ src/cameras/map/index.jsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 import GoogleMapReact from 'google-map-react'
 
-import { useSettings } from 'settings/hooks/use-settings'
 import { useStyles } from 'cameras/map/style'
 import { useItems } from 'cameras/list/items/hooks/use-item'
 import { Marker } from 'cameras/map/marker'
+import { useMapProps } from 'cameras/map/hooks/use-map-props'
 
 export const Map = () => <Grid container className={useStyles().grid}>
-  <GoogleMapReact defaultCenter={{
-    lat: useSettings().google.mapProps.latitude,
-    lng: useSettings().google.mapProps.longitude,
-  }} defaultZoom={useSettings().google.mapProps.zoom} bootstrapURLKeys={{ key: useSettings().google.map }}>
+  <GoogleMapReact {...useMapProps()}>
     {useItems().map((item, index) => <Marker item={item} key={index} lat={item.latitude} lng={item.longitude} />)}
   </GoogleMapReact>
 </Grid>
