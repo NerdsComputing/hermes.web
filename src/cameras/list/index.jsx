bbb5ccaf0369d8e035834fa80fda0311b76558da
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-18 13:53:48 +0300

    [HER-128] Follow-up Change index in list

--- src/cameras/list/index.jsx
+++ src/cameras/list/index.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import { List } from '@material-ui/core'
 
 import { Search } from 'cameras/search'
-import { Items } from 'cameras/list/items'
-import { useListStyle } from 'cameras/list/hooks/use-list-style'
 import { Behaviour } from 'cameras/behaviour'
+import { ItemIds } from 'cameras/list/item-ids'
 
 export const Ids = () => <>
   <Behaviour />
   <Search />
-  <List classes={useListStyle()}>
-    <Items />
-  </List>
+  <ItemIds />
 </>
 
