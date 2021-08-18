e53852ee6b84430705ea6faf0f35524a5d9873da
bianca <bia.topircean@gmail.com>
2021-08-18 10:45:56 +0300

    [HER-152] Follow-up Use lowercase

--- src/detection/card/items/date-format.js
+++ src/detection/card/items/date-format.js
@@ -1,4 +1,4 @@
-export const DateFormat = timestamp => {
+export const dateFormat = timestamp => {
   const date = new Date(timestamp)
 
   return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${
--- src/detection/card/items/timestamp.jsx
+++ src/detection/card/items/timestamp.jsx
@@ -4,14 +4,13 @@ import { WatchLater } from '@material-ui/icons'
 import PropTypes from 'prop-types'
 
 import { useStyles } from 'detection/card/items/style'
-import { DateFormat } from 'detection/card/items/date-format'
+import { dateFormat } from 'detection/card/items/date-format'
 
 export const Timestamp = ({ item }) => <Box classes={useStyles()}>
   <WatchLater />
   <Typography variant={'h6'} align={'center'} className={useStyles().contentText}>
-    {DateFormat(item.timestamp)}
+    {dateFormat(item.timestamp)}
   </Typography>
 </Box>
 
 Timestamp.propTypes = { item: PropTypes.object.isRequired }
-
