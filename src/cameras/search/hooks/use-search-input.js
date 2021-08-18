d68584fab3b44e9c4fa3cbcb3a9f1b9ddf4cf08d
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-18 13:50:55 +0300

    [HER-128] Follow-up Make one liner

--- src/cameras/search/hooks/use-search-input.js
+++ src/cameras/search/hooks/use-search-input.js
@@ -1,10 +1,9 @@
-import { useContext } from 'cameras/context'
 import { useCallback } from 'react'
+
+import { useContext } from 'cameras/context'
 
 export const useSearchInput = () => {
   const { setSearchPhrase, searchInput } = useContext()
 
-  return useCallback(() => {
-    setSearchPhrase(searchInput)
-  }, [setSearchPhrase, searchInput])
+  return useCallback(() => setSearchPhrase(searchInput), [setSearchPhrase, searchInput])
 }
