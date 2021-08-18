c28cf5f247b05f7d5169725aafbf0b98b90c4266
bianca <bia.topircean@gmail.com>
2021-08-18 10:47:04 +0300

    [HER-152] Follow-up Reorder imports

--- src/detection/card/title/index.jsx
+++ src/detection/card/title/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { CardHeader } from '@material-ui/core'
+import PropTypes from 'prop-types'
 
 import { useStyles } from 'detection/card/title/style'
-import PropTypes from 'prop-types'
 
 export const Title = ({ id }) => <CardHeader title={`Detection ${id}`} classes={useStyles()} />
 
