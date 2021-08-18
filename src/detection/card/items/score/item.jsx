942804a42657bc780bdfa6b8e2b20f549edd2bac
bianca <bia.topircean@gmail.com>
2021-08-18 14:16:00 +0300

    [HER-152] Follow-up Remove items const

--- src/detection/card/items/score/item.jsx
+++ src/detection/card/items/score/item.jsx
@@ -4,10 +4,7 @@ import { Typography } from '@material-ui/core'
 import { useStyles } from 'detection/card/items/style'
 import { useItem } from 'detection/card/use-item'
 
-export const Item = () => {
-  const item = useItem()
+export const Item = () => <Typography variant={'h6'} className={useStyles().contentText}>
+  {`Score: ${useItem().score * 100}`}
+</Typography>
 
-  return <Typography variant={'h6'} className={useStyles().contentText}>
-    {`Score: ${item.score * 100}`}
-  </Typography>
-}
--- src/detection/card/items/timestamp/item.jsx
+++ src/detection/card/items/timestamp/item.jsx
@@ -5,10 +5,7 @@ import { useStyles } from 'detection/card/items/style'
 import { dateFormat } from 'detection/card/items/date-format'
 import { useItem } from 'detection/card/use-item'
 
-export const Item = () => {
-  const item = useItem()
+export const Item = () => <Typography variant={'h6'} align={'center'} className={useStyles().contentText}>
+  {dateFormat(useItem().timestamp)}
+</Typography>
 
-  return <Typography variant={'h6'} align={'center'} className={useStyles().contentText}>
-    {dateFormat(item.timestamp)}
-  </Typography>
-}
