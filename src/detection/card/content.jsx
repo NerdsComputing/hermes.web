6391df1f8fe66e9cd9463be3674638d7c2d419ee
bianca <bia.topircean@gmail.com>
2021-08-18 14:13:43 +0300

    [HER-152] Reorder imports

--- src/detection/card/content.jsx
+++ src/detection/card/content.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-import { Score } from 'detection/card/items/score'
-import { Timestamp } from 'detection/card/items/timestamp'
-import { Class } from 'detection/card/items/class'
 import { CardContent } from 'detection/card/card-content'
+import { Score } from 'detection/card/items/score'
+import { Class } from 'detection/card/items/class'
+import { Timestamp } from 'detection/card/items/timestamp'
 
 export const Content = () => <CardContent>
   <Score />
