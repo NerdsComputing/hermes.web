0bf2b06a4b425042629a0e2a33b85d5153704f79
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-08-18 17:11:59 +0300

    [HER-128] Follow-up Update imports

--- src/cameras/sidebar/index.jsx
+++ src/cameras/sidebar/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import * as Material from '@material-ui/core'
 
-import { Ids } from 'cameras/list'
-import { Header } from 'cameras/header'
+import { Header } from 'cameras/sidebar/header'
+import { Ids } from 'cameras/sidebar/ids'
 
 export const Sidebar = () => <Material.Grid item xs={12} md={3}>
   <Header />
