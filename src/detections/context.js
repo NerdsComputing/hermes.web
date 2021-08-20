69ea700596e30c125d0074bbc882160750720c79
Codrut <constantinescu321@gmail.com>
2021-08-20 14:25:23 +0300

    [HER-144] Create formik

--- src/detections/context.js
+++ src/detections/context.js
@@ -1,12 +1,16 @@
 import React, { useContext as useReactContext, useState } from 'react'
+import { useFormik } from 'formik'
+
+import { useSetFormik } from 'detections/hooks/use-set-formik'
 
 export const Context = React.createContext({})
 
 export const useContext = () => useReactContext(Context)
 
 export const useDefaultContext = () => {
-  const [pageIndex, setPageIndex] = useState(0)
   const [pageSize, setPageSize] = useState(10)
+  const [pageIndex, setPageIndex] = useState(0)
+  const formik = useFormik({ ...useSetFormik(pageSize, pageIndex) })
 
-  return { pageIndex, setPageIndex, pageSize, setPageSize }
+  return { pageIndex, setPageIndex, pageSize, setPageSize, ...formik }
 }
