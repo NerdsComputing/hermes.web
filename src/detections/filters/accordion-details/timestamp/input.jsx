93ddc23c651a85188707507810363bd99703660b
Codrut <constantinescu321@gmail.com>
2021-08-20 15:03:44 +0300

    [HER-144] Add error component

--- src/detections/filters/accordion-details/timestamp/input.jsx
+++ src/detections/filters/accordion-details/timestamp/input.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { MuiPickersUtilsProvider } from '@material-ui/pickers'
-import DateFnsUtils from '@date-io/date-fns'
 
 import { InputWrapper } from 'detections/filters/accordion-details/input-wrapper'
 import { Date } from 'detections/filters/accordion-details/timestamp/date'
+import { Error } from 'detections/filters/accordion-details/error'
+import { useContext } from 'detections/context'
+import { useCheckError } from 'detections/filters/accordion-details/error/use-check-error'
 
-export const Input = () => <InputWrapper>
-  <MuiPickersUtilsProvider utils={DateFnsUtils}>
-    <Date label={'From'} />
-    <Date label={'To'} />
-  </MuiPickersUtilsProvider>
-</InputWrapper>
+export const Input = () => {
+  const { errors } = useContext()
+
+  return <InputWrapper>
+    <Date label={'From'} name={'greaterEqualThan'} />
+    <Date label={'To'} name={'lesserEqualThan'} />
+    {useCheckError(errors.timestamp) && <Error errorMessage={'First date must be lower or equal than second!'} />}
+  </InputWrapper>
+}
