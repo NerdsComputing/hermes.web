a086e781805dc6a735d234ff2404d933fb56464c
Codrut <constantinescu321@gmail.com>
2021-08-25 22:42:47 +0300

    [HER-144] Follow-up Remove unnecessary space

--- src/detections/filters/accordion-details/class/use-props.js
+++ src/detections/filters/accordion-details/class/use-props.js
@@ -1,5 +1,4 @@
 import { useContext } from 'detections/context'
-
 import { useSetInput } from 'detections/filters/accordion-details/class/use-set-input'
 import { useCheckErrors } from 'detections/filters/accordion-details/class/use-check-errors'
 
