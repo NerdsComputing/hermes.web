eac119881e32468ccc2b68cb7c9ff68cad7ee227
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-09-15 15:56:59 +0300

    [HER-155] Change from material to grid or box

--- src/cameras/index.jsx
+++ src/cameras/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import * as Material from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 
 import { Content } from 'cameras/content'
 import { useDefaultContext, Context } from 'cameras/context'
 import { Sidebar } from 'cameras/sidebar'
 
 export const Cameras = () => <Context.Provider value={useDefaultContext()}>
-  <Material.Grid container>
+  <Grid container>
     <Sidebar />
     <Content />
-  </Material.Grid>
+  </Grid>
 </Context.Provider>
 
--- src/cameras/sidebar/index.jsx
+++ src/cameras/sidebar/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import * as Material from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 
 import { Header } from 'cameras/sidebar/header'
 import { Ids } from 'cameras/sidebar/ids'
 
-export const Sidebar = () => <Material.Grid item xs={12} md={3}>
+export const Sidebar = () => <Grid item xs={12} md={3}>
   <Header />
   <Ids />
-</Material.Grid>
+</Grid>
