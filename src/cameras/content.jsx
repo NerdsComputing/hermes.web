bede2cfc5cb96773df7ba2cc1e9e4c46cdc50a67
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-09-16 12:05:26 +0300

    [HER-155] Put the livestream component into a box

--- src/cameras/content.jsx
+++ src/cameras/content.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import YouTube from 'react-youtube'
 import { Box, Grid } from '@material-ui/core'
 
 import { Map } from 'cameras/map'
-import { settings } from 'settings'
-import { useLivestreamProps } from 'cameras/hooks/use-livestream-props'
+import { Livestream } from 'cameras/livestream'
+import { useLivestreamPadding } from 'cameras/hooks/use-livestream-padding'
 
 export const Content = () => <Grid xs={12} sm={11} md={9} lg={8}>
   <Box pt={3} style={{ height: '80vh', width: '100%' }}>
     <Map />
   </Box>
-  <Grid {...useLivestreamProps()}>
-    <YouTube videoId={settings().sources[0]} />
-  </Grid>
+  <Box pt={3} {...useLivestreamPadding()}>
+    <Livestream />
+  </Box>
 </Grid>
 
