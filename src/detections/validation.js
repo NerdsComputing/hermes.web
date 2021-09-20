bf00e2291d5363bfad513af231d5422e51a1cee0
Codrut <constantinescu321@gmail.com>
2021-09-20 16:56:42 +0300

    [HER-144] Follow-up Change validation for date

--- src/detections/validation.js
+++ src/detections/validation.js
@@ -17,10 +17,10 @@ export const validation = Yup.object({
   timestamp: Yup.object({
     greaterEqualThan: Yup.date()
       .max(Date(), 'Date cannot be in future')
-      .min('June 1, 2021 23:59:59', 'Date cannot be older than June 1 2021'),
+      .min('January 1, 2021 00:00:00', 'Date cannot be older than January 1 2021'),
     lesserEqualThan: Yup.date()
       .max(Date(), 'Date cannot be in future')
-      .min('June 1, 2021 23:59:59', 'Date cannot be older than June 1 2021'),
+      .min('January 1, 2021 00:00:00', 'Date cannot be older than January 1 2021'),
   })
     .test('timestamp-test', 'timestamp-error', timestamp => checkValues(timestamp)),
 })
