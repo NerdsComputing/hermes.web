ccc586df538142bfa30acaae1b53709bbd2c8e70
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-09-21 13:13:55 +0300

    [HER-133] Change style of the marker and add the info style

--- src/cameras/map/style.js
+++ src/cameras/map/style.js
@@ -1,14 +1,9 @@
 import { makeStyles } from '@material-ui/core'
 
 const markerStyle = {
-  border: '5px solid #f44336',
-  borderRadius: 20,
-  backgroundColor: 'white',
-  textAlign: 'center',
   color: '#3f51b5',
-  fontSize: 16,
-  fontWeight: 'bold',
   padding: 4,
+  cursor: 'pointer',
 }
 const markerPosition = {
   position: 'absolute',
@@ -22,4 +17,15 @@ const grid = {
   height: '80vh',
   width: '100%',
 }
-export const useStyles = makeStyles({ markerPosition, grid })
+const infoWindowStyle = {
+  position: 'relative',
+  bottom: 150,
+  left: '-45px',
+  width: 220,
+  backgroundColor: 'white',
+  boxShadow: '0 2px 7px 1px rgba(0, 0, 0, 0.3)',
+  padding: 10,
+  fontSize: 14,
+  zIndex: 100,
+}
+export const useStyles = makeStyles({ markerPosition, grid, infoWindowStyle })
