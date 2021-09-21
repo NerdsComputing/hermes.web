6cb8f9537031a0d0bed41733d2b98624f2abc616
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-09-21 13:13:00 +0300

    [HER-133] Add the tooltip to marker

---
+++ src/cameras/map/marker/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import LocationOnIcon from '@material-ui/icons/LocationOn'
+
+import { useStyles } from 'cameras/map/style'
+import { Info } from 'cameras/map/marker/info'
+
+export const Marker = ({ item }) => <>
+  <div className={useStyles().markerPosition}>
+    <LocationOnIcon fontSize={'large'} />
+  </div>
+  {item.show && <Info item={item} />}
+</>
+
+Marker.propTypes = { item: PropTypes.string.isRequired }
