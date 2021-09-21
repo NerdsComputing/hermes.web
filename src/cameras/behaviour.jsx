4a2a252a15c1be4e7593621298ef585a33fb5ffe
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-09-21 13:07:59 +0300

    [HER-133] Set all the showing fields to false at first

--- src/cameras/behaviour.jsx
+++ src/cameras/behaviour.jsx
@@ -1,7 +1,9 @@
 import { useFetchCameras } from 'cameras/hooks/use-fetch-cameras'
+import { useAddShowField } from 'cameras/map/hooks/use-add-show-filed'
 
 export const Behaviour = () => {
   useFetchCameras()
+  useAddShowField()
 
   return ''
 }
