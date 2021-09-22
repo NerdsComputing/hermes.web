c433a6bcba0303688866ef387e41e3ce24bee76c
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-09-22 13:42:20 +0300

    [HER-133] Follow-up Add item to proptypes to latitude

--- src/cameras/map/marker/info/latitude.jsx
+++ src/cameras/map/marker/info/latitude.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Box, Typography } from '@material-ui/core'
 import PropTypes from 'prop-types'
 
+import { Item } from 'cameras/map/marker/item-props'
+
 export const Latitude = ({ item }) => <Box>
   <Typography variant={'h10'} align={'center'}>
     {`Latitude: ${item.latitude}`}
   </Typography>
 </Box>
 
-Latitude.propTypes = { item: PropTypes.object.isRequired }
+Latitude.propTypes = { item: PropTypes.objectOf(Item).isRequired }
