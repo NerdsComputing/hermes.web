078b81855480d18ab312ba158dcd03009f34ed81
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-09-22 15:14:09 +0300

    [HER-161] Add hooks to map lat and lng

--- src/cameras/map/hooks/use-map-props.js
+++ src/cameras/map/hooks/use-map-props.js
@@ -1,8 +1,10 @@
 import { useSettings } from 'settings/hooks/use-settings'
+import { useCalculateLat } from 'cameras/map/hooks/use-calculate-lat'
+import { useCalculateLng } from 'cameras/map/hooks/use-calculate-lng'
 import { useShowMarker } from 'cameras/map/hooks/use-show-marker'
 
 export const useMapProps = () => ({
-  defaultCenter: { lat: useSettings().google.map.latitude, lng: useSettings().google.map.longitude },
+  defaultCenter: { lat: useCalculateLat(), lng: useCalculateLng() },
   defaultZoom: useSettings().google.map.zoom,
   bootstrapURLKeys: { key: useSettings().google.map.id },
   onChildClick: useShowMarker(),
