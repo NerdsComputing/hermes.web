d9d5cb5c94f3e7aaa4f9bf501f4703e559d92194
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-09-24 13:46:16 +0300

    [HER-161] Follow-up Fix the tooltip

--- src/cameras/map/content.jsx
+++ src/cameras/map/content.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
-import GoogleMapReact from 'google-map-react'
-
-import { useStyles } from 'cameras/map/style'
-import { useItems } from 'cameras/list/items/hooks/use-item'
 import { Marker } from 'cameras/map/marker'
-import { useMapProps } from 'cameras/map/hooks/use-map-props'
+import { ShowMap } from 'cameras/map/show-map'
+import { useContext } from 'cameras/context'
 
-export const Content = () => <Grid container className={useStyles().grid}>
-  <GoogleMapReact {...useMapProps()}>
-    {useItems().map((item, index) => <Marker item={item} key={index} lat={item.latitude} lng={item.longitude} />)}
-  </GoogleMapReact>
-</Grid>
+export const Content = () => {
+  const { items } = useContext()
+
+  return <ShowMap>
+    {
+      items.map((item, index) => <Marker item={item} key={index} lat={item.latitude} lng={item.longitude} />)
+    }
+  </ShowMap>
+}
