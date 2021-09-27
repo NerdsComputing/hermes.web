d4ded8214b230fb0b4604ebe82396597515bed68
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-09-27 15:50:17 +0300

    [HER-162] Use the zoom on map

--- src/cameras/map/hooks/use-map-props.js
+++ src/cameras/map/hooks/use-map-props.js
@@ -2,10 +2,11 @@ import { useSettings } from 'settings/hooks/use-settings'
 import { useShowMarker } from 'cameras/map/hooks/use-show-marker'
 import { useLatitude } from 'cameras/map/hooks/use-latitude'
 import { useLongitude } from 'cameras/map/hooks/use-longitude'
+import { useZoom } from 'cameras/map/hooks/use-zoom'
 
 export const useMapProps = () => ({
   defaultCenter: { lat: useLatitude(), lng: useLongitude() },
-  defaultZoom: useSettings().google.map.zoom,
+  defaultZoom: useZoom(),
   bootstrapURLKeys: { key: useSettings().google.map.id },
   onChildClick: useShowMarker(),
 })
