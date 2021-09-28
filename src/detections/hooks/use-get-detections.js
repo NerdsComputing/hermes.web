9173a11cb40079738249723e07d92ae503863b91
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-09-28 17:43:30 +0300

    [HER-165] Follow-up Use the attempt`s parameters

--- src/detections/hooks/use-get-detections.js
+++ src/detections/hooks/use-get-detections.js
@@ -2,14 +2,12 @@ import { useCallback } from 'react'
 import { useDispatch } from 'react-redux'
 
 import { actions } from 'detections/slices/get'
-import { useContext } from 'detections/context'
+import { useParameters } from 'detections/hooks/use-parameters'
 
 export const useGetDetections = () => {
   const dispatch = useDispatch()
   const { attempt } = actions
-  const { pageIndex, pageSize, cameraId } = useContext()
+  const dataFetch = { ...useParameters() }
 
-  return useCallback(() => {
-    dispatch(attempt({ cameraIds: [cameraId], pagination: { pageIndex, pageSize } }))
-  }, [dispatch, attempt, pageIndex, pageSize, cameraId])
+  return useCallback(() => dispatch(attempt(dataFetch)), [dispatch, attempt, dataFetch])
 }
