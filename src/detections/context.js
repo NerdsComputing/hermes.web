a7a4554c7a98d11fd787d452c623197595ce1d62
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-09-28 13:58:07 +0300

    [HER-165] Put the cameraId in context

--- src/detections/context.js
+++ src/detections/context.js
@@ -2,15 +2,17 @@ import React, { useContext as useReactContext, useState } from 'react'
 import { useFormik } from 'formik'
 
 import { useSetFormik } from 'detections/hooks/use-set-formik'
+import { useSplit } from 'detections/hooks/use-split'
 
 export const Context = React.createContext({})
 
 export const useContext = () => useReactContext(Context)
 
 export const useDefaultContext = () => {
+  const cameraId = useSplit()
   const [pageSize, setPageSize] = useState(10)
   const [pageIndex, setPageIndex] = useState(0)
   const formik = useFormik(useSetFormik(pageSize, pageIndex))
 
-  return { pageIndex, setPageIndex, pageSize, setPageSize, ...formik }
+  return { pageIndex, setPageIndex, pageSize, setPageSize, ...formik, cameraId }
 }
