9f34d4d770b86f30dc3ac4d0d1c2a4e692a0a526
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-09-29 15:08:23 +0300

    [HER-165] Follow-up Rename from splits to keys

--- src/detections/hooks/use-camera-filter.js
+++ src/detections/hooks/use-camera-filter.js
@@ -1,10 +1,11 @@
 import { useLocation } from 'react-router-dom'
+
 import { useIndex } from 'detections/hooks/use-index'
 
 export const useCameraFilter = () => {
   const link = useLocation()
   const cameraId = useIndex()
-  const splits = link.search.split(/[?,&,=]/)
+  const keys = link.search.split(/[?,&,=]/)
 
-  return link.search && cameraId > 0 ? [splits[cameraId + 1]] : [null]
+  return link.search && cameraId > 0 ? [keys[cameraId + 1]] : [null]
 }
--- src/detections/hooks/use-index.js
+++ src/detections/hooks/use-index.js
@@ -2,7 +2,7 @@ import { useLocation } from 'react-router-dom'
 
 export const useIndex = () => {
   const link = useLocation()
-  const splits = link.search.split(/[?,&,=]/)
+  const keys = link.search.split(/[?,&,=]/)
 
-  return splits.indexOf('cameraId')
+  return keys.indexOf('cameraId')
 }
