250092cd13bff92a6940669b6a9ac0d3939e9bb7
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-01 10:46:00 +0300

    [HER-167] Update south-west to accept null cases

--- src/cameras/map/hooks/use-south-west.js
+++ src/cameras/map/hooks/use-south-west.js
@@ -2,7 +2,7 @@ import { useContext } from 'cameras/context'
 
 export const useSouthWest = () => ({
   sw: {
-    lat: { ...useContext().items[useContext().items.length - 1] }.latitude,
-    lng: { ...useContext().items[useContext().items.length - 1] }.longitude,
+    lat: { ...useContext().items[useContext().items.length - 1] }.latitude || 1,
+    lng: { ...useContext().items[useContext().items.length - 1] }.longitude || 1,
   },
 })
