a0841f3dd2b9437a25460eef91f078fba2ee5b98
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-01 16:46:39 +0300

    [HER-166] Follow-up Add callback at onClick action

--- src/detection/card/items/camera/hooks/use-on-click.js
+++ src/detection/card/items/camera/hooks/use-on-click.js
@@ -1,4 +1,11 @@
+import { useCallback } from 'react'
+
 import { useNavigation } from 'detections/table/hooks/use-navigation'
 import { useItem } from 'detection/card/use-item'
 
-export const useOnClick = () => ({ onClick: useNavigation(`/cameras?id=${useItem().camera.id}`) })
+export const useOnClick = () => {
+  const items = useItem()
+  const navigate = useNavigation(`/cameras?id=${items.camera.id}`)
+
+  return useCallback(navigate, [navigate])
+}
