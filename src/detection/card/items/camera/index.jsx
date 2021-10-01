e694f4e7ad07945ecd3cb0fe748edc61a6fa234c
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-01 16:45:36 +0300

    [HER-166] Follow-up Remove Item component

--- src/detection/card/items/camera/index.jsx
+++ src/detection/card/items/camera/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import CameraAltRounded from '@material-ui/icons/CameraAltRounded'
-import { Box } from '@material-ui/core'
+import { Box, Typography } from '@material-ui/core'
 
 import { useStyles } from 'detection/card/items/style'
-import { Item } from 'detection/card/items/camera/item'
+import { useTypography } from 'detection/card/items/camera/hooks/use-typography'
+import { useLink } from 'detection/card/items/camera/hooks/use-link'
+import { useItem } from 'detection/card/use-item'
 
 export const Camera = () => <Box classes={useStyles()}>
   <CameraAltRounded />
-  <Item />
+  <Typography {...useTypography()}>Camera:</Typography>
+  <Typography {...useLink()}>{`${useItem().camera.id}`}</Typography>
 </Box>
