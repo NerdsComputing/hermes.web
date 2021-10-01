4a9060cb4c9f1c22c5835f827288cf23ae187fc1
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-01 10:45:42 +0300

    [HER-167] Update north-est to accept null cases

--- src/cameras/map/hooks/use-north-est.js
+++ src/cameras/map/hooks/use-north-est.js
@@ -2,7 +2,7 @@ import { useContext } from 'cameras/context'
 
 export const useNorthEst = () => ({
   ne: {
-    lat: { ...useContext().items[0] }.latitude,
-    lng: { ...useContext().items[0] }.longitude,
+    lat: { ...useContext().items[0] }.latitude || 1,
+    lng: { ...useContext().items[0] }.longitude || 1,
   },
 })
