bc28646f1098681879ce8788948a80ffe525fb7b
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-01 10:40:29 +0300

    [HER-167] Create hook for finding the index of 'id'

---
+++ src/cameras/hooks/use-index.js
@@ -0,0 +1,8 @@
+import { useLocation } from 'react-router-dom'
+
+export const useIndex = () => {
+  const link = useLocation()
+  const keys = link.search.split(/[?,&,=]/)
+
+  return keys.indexOf('id')
+}
