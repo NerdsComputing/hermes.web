cc3b85297efedfb5da8e52756da5ac36c1bbfd82
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-06 19:07:26 +0300

    [HER-163] Add total count component for info tooltip

--- src/cameras/map/marker/info/index.jsx
+++ src/cameras/map/marker/info/index.jsx
@@ -8,11 +8,13 @@ import { Longitude } from 'cameras/map/marker/info/longitude'
 import { useStyles } from 'cameras/map/style'
 import { Item } from 'cameras/map/marker/item-props'
 import { useNavigation } from 'detections/table/hooks/use-navigation'
+import { TotalCounts } from 'cameras/map/marker/info/total-counts'
 
 export const Info = ({ item }) => <Box className={useStyles().infoWindowStyle}>
   <Id item={item} />
   <Latitude item={item} />
   <Longitude item={item} />
+  <TotalCounts item={item} />
   <Button onClick={useNavigation(`/detections?cameraId=${item.id}`)}> See more </Button>
 </Box>
 
