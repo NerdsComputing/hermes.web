ff73e4db086b88f62ef0ad50c647a8b74aa6b647
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-06 17:10:27 +0300

    [HER-169] Add the sgv icon on footer

--- src/core/layout/footer/content.jsx
+++ src/core/layout/footer/content.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Link, Typography } from '@material-ui/core'
 
 import { useStyles } from 'core/layout/footer/content/style'
+import Heart from 'core/layout/icons/favorite_border_black_24dp.svg'
+import { Link, Typography } from '@material-ui/core'
 
 export const Content = () => <Typography className={useStyles().content}>
-  Made with &#x2764; by
+  Made with
+  {' '}
+  <img src={Heart} alt={'heart'} className={useStyles().image} />
+  {' '}
+  by
   <Link color={'inherit'} href={'/about'}> the Hermes team</Link>
 </Typography>
-
