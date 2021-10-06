1209975b35b629702af7981d2c662715ffc414e3
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-06 19:12:30 +0300

    [HER-163] Add hook that counts detection when initialize the cameras

--- src/cameras/map/hooks/use-init-items.js
+++ src/cameras/map/hooks/use-init-items.js
@@ -2,11 +2,14 @@ import { useEffect } from 'react'
 
 import { useContext } from 'cameras/context'
 import { useItems } from 'cameras/map/hooks/use-items'
+import { useDetectionsCount } from 'cameras/map/hooks/use-detections-count'
 
 export const useInitItems = () => {
   const { setItems } = useContext()
   const items = useItems()
+  const numb = useDetectionsCount()
+
   useEffect(() => {
-    items.length && setItems(items.map(item => ({ ...item, show: false })))
-  }, [setItems, items])
+    items.length && setItems(items.map(camera => ({ ...camera, show: camera.show || false, detections: numb(camera) })))
+  }, [setItems, items, numb])
 }
