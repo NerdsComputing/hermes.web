18eb5225e41405246fd7708cc26698274bbe74c6
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-06 17:11:56 +0300

    [HER-169] Add the style of the content from the footer

--- src/core/layout/footer/content/style.js
+++ src/core/layout/footer/content/style.js
@@ -1,3 +1,14 @@
 import { makeStyles } from '@material-ui/core'
 
-export const useStyles = makeStyles(theme => ({ content: { color: 'white' } }))
+const content = {
+  color: 'black',
+  font: 'normal normal normal 16px/19px Roboto',
+}
+
+const image = {
+  filter: 'invert(55%) sepia(83%) saturate(351%) hue-rotate(111deg) brightness(86%) contrast(87%)',
+  height: '17px',
+  marginBottom: '-3px',
+}
+
+export const useStyles = makeStyles({ content, image })
