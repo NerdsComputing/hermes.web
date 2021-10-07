c0d2568b421ba581d22ae62b5cb4d332535a9b9d
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-07 19:07:14 +0300

    [HER-169] Follow-up Move style in content folder

--- src/core/layout/footer/content/style.js
+++ src/core/layout/footer/content/style.js
@@ -1,11 +1,5 @@
 import { makeStyles } from '@material-ui/core'
 
-const root = {
-  width: '100%',
-  backgroundColor: '#F5F7FA',
-  minHeight: '72px',
-  opacity: '1',
-}
 const content = {
   color: 'black',
   font: 'normal normal normal 16px/19px Roboto',
@@ -17,4 +11,4 @@ const image = {
   marginBottom: '-3px',
 }
 
-export const useStyles = makeStyles({ root, content, image })
+export const useStyles = makeStyles({ content, image })
