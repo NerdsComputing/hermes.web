d399040c0045a3a06d195a9a6d254f0455027f56
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-07 16:22:14 +0300

    [HER-163] Follow-up Extract show initial value into a separate function

--- src/cameras/map/hooks/use-init-items.js
+++ src/cameras/map/hooks/use-init-items.js
@@ -4,12 +4,14 @@ import { useContext } from 'cameras/context'
 import { useItems } from 'cameras/map/hooks/use-items'
 import { useDetectionsCount } from 'cameras/map/hooks/use-detections-count'
 
+const initShow = camera => camera.show || false
+
 export const useInitItems = () => {
   const { setItems } = useContext()
   const items = useItems()
-  const numb = useDetectionsCount()
+  const count = useDetectionsCount()
 
   useEffect(() => {
-    items.length && setItems(items.map(camera => ({ ...camera, show: camera.show || false, detections: numb(camera) })))
-  }, [setItems, items, numb])
+    items.length && setItems(items.map(camera => ({ ...camera, show: initShow(camera), detections: count(camera) })))
+  }, [setItems, items, count])
 }
