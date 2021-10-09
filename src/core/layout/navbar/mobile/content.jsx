6db12b46698265e0f8a89dfcf27fa2654cb8f17f
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-09 18:13:07 +0300

    [HER-157] Make appbar transparent

--- src/core/layout/navbar/mobile/content.jsx
+++ src/core/layout/navbar/mobile/content.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { AppBar, Grid } from '@material-ui/core'
 
-import { Home } from 'core/layout/navbar/links/home'
 import { Icon } from 'core/layout/navbar/mobile/icon'
+import { LogoPhone } from 'core/layout/navbar/logo/logo-phone'
 
-export const Content = () => <AppBar>
+export const Content = () => <AppBar color={'transparent'} style={{ height: 95 }}>
   <Grid container justify={'space-between'} alignItems={'center'}>
-    <Home />
+    <LogoPhone />
     <Icon />
   </Grid>
 </AppBar>
