9c2801bb63cfdae3ec383dd945274aec03a6936d
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-09 18:05:11 +0300

    [HER-157] Move login into header component

--- src/core/layout/navbar/header/index.jsx
+++ src/core/layout/navbar/header/index.jsx
@@ -4,9 +4,13 @@ import { Grid } from '@material-ui/core'
 import { Home } from 'core/layout/navbar/links/home'
 import { NavigateToCameras } from 'core/layout/navbar/links/cameras'
 import { NavigateToDetections } from 'core/layout/navbar/links/detections'
+import { Login } from 'login'
+import { useHeaderStyle } from 'core/layout/navbar/header/hooks/use-header-style'
 
-export const Header = () => <Grid item container xs={12} sm={8} md={9} lg={10}>
+export const Header = () => <Grid item container xs={12} sm={8} md={9} lg={10} classes={useHeaderStyle()}>
   <Home />
   <NavigateToDetections />
   <NavigateToCameras />
+  <Login />
 </Grid>
+
