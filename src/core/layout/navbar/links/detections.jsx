db9206e48badbe48b3cdd455b4446f4578d8da6f
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-09 18:22:07 +0300

    [HER-157] Add props on detections component

--- src/core/layout/navbar/links/detections.jsx
+++ src/core/layout/navbar/links/detections.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import * as Material from '@material-ui/core'
 
-import { useStyles } from 'core/layout/navbar/links/styles'
 import { useNavigation } from 'core/layout/navbar/links/hooks/use-navigation'
+import { Line } from 'core/layout/navbar/links/line'
+import { useDetectionsProps } from 'core/layout/navbar/links/hooks/use-detections-props'
 
 export const NavigateToDetections = () => <Material.Grid item>
   <Material.Box px={2}>
-    <Material.Link onClick={useNavigation('/detections')} variant={'h6'} classes={useStyles()}>
+    <Material.Link onClick={useNavigation('/detections')} {...useDetectionsProps()}>
       Detections
     </Material.Link>
+    <Line path={'/detections'} />
   </Material.Box>
 </Material.Grid>
