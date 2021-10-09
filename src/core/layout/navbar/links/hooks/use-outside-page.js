2c9e1a72e8992f532d98f0283c00a868b51dd621
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-09 18:07:56 +0300

    [HER-157] Add style of a header element when we are not on page

---
+++ src/core/layout/navbar/links/hooks/use-outside-page.js
@@ -0,0 +1,8 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  color: '#52606C',
+  paddingLeft: '5px',
+  cursor: 'pointer',
+}
+export const useOutsidePage = makeStyles({ root })
