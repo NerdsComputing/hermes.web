32c026c7523b6395edb7fff9ee2876a2982a4804
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-09 18:22:19 +0300

    [HER-157] Add props on cameras component

--- src/core/layout/navbar/links/cameras.jsx
+++ src/core/layout/navbar/links/cameras.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import * as Material from '@material-ui/core'
 
-import { useStyles } from 'core/layout/navbar/links/styles'
 import { useNavigation } from 'core/layout/navbar/links/hooks/use-navigation'
+import { Line } from 'core/layout/navbar/links/line'
+import { useCamerasProps } from 'core/layout/navbar/links/hooks/use-cameras-props'
 
 export const NavigateToCameras = () => <Material.Grid item>
   <Material.Box px={2}>
-    <Material.Link onClick={useNavigation('/cameras')} variant={'h6'} classes={useStyles()}>
+    <Material.Link onClick={useNavigation('/cameras')} {...useCamerasProps()}>
       Cameras
     </Material.Link>
+    <Line path={'/cameras'} />
   </Material.Box>
 </Material.Grid>
