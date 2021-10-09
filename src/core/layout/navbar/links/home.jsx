8b47aa1cf630a6da8d7dfed283c5c1b5d01a2af2
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-09 18:21:40 +0300

    [HER-157] Add props on home component

--- src/core/layout/navbar/links/home.jsx
+++ src/core/layout/navbar/links/home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import * as Material from '@material-ui/core'
 
-import { useStyles } from 'core/layout/navbar/links/styles'
 import { useNavigation } from 'core/layout/navbar/links/hooks/use-navigation'
+import { Line } from 'core/layout/navbar/links/line'
+import { useHomeProps } from 'core/layout/navbar/links/hooks/use-home-props'
 
 export const Home = () => <Material.Grid item>
   <Material.Box px={2} pl={4}>
-    <Material.Link onClick={useNavigation('/')} variant={'h6'} classes={useStyles()}>
+    <Material.Link onClick={useNavigation('/')} {...useHomeProps()}>
       Hermes
     </Material.Link>
+    <Line path={'/'} />
   </Material.Box>
 </Material.Grid>
