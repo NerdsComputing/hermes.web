2b0f79a6e0fa33c6b252944475946042460442aa
andreea-dumitrascu <72592844+andreea-dumitrascu@users.noreply.github.com>
2021-10-11 14:10:38 +0300

    [HER-169] Update the design of footer (#57)
    * [HER-169] Add the svg heart icon
    
    * [HER-169] Add the sgv icon on footer
    
    * [HER-169] Add the style of the content from the footer
    
    * [HER-169] Change the style of the footer
    
    * [HER-169] Follow-up Change from cassName to classes
    
    * [HER-169] Follow-up Delete the content folder
    
    * [HER-169] Follow-up Move content to content and rename it
    
    * [HER-169] Follow-up Move style in content folder

--- src/core/layout/footer/content/index.jsx
+++ src/core/layout/footer/content/index.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { Link, Typography } from '@material-ui/core'
 
 import { useStyles } from 'core/layout/footer/content/style'
+import Heart from 'core/layout/icons/favorite_border_black_24dp.svg'
 
 export const Content = () => <Typography className={useStyles().content}>
-  Made with &#x2764; by
+  Made with
+  {' '}
+  <img src={Heart} alt={'heart'} className={useStyles().image} />
+  {' '}
+  by
   <Link color={'inherit'} href={'/about'}> the Hermes team</Link>
 </Typography>
-
