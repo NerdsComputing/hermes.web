77b918a877287c58e51509b603ca3ba9308a043a
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-12 14:34:34 +0300

    [HER-157] Follow-up Erase links props

--- src/core/layout/navbar/links/hooks/use-cameras-props.js
+++
@@ -1,7 +0,0 @@
-import { useStyles } from 'core/layout/navbar/links/hooks/use-styles'
-
-export const useCamerasProps = () => ({
-  variant: 'h6',
-  classes: useStyles('/cameras'),
-  underline: 'none',
-})
