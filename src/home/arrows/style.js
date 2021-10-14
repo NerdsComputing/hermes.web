f8efd0aab37257cbd88c8569bd28ebd094e5fad2
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-14 19:25:18 +0300

    [HER-170] Delete the carousel

--- src/home/arrows/style.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@material-ui/core'
-
-export const useStyles = makeStyles(() => ({
-  arrow: {
-    paddingTop: '22%',
-    paddingLeft: '1%',
-    paddingRight: '1%',
-  },
-}))
