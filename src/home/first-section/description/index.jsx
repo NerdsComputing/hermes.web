96d22493fc08348f388225222cecb063a2e484ac
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-14 19:35:06 +0300

    [HER-170] Create the description component

---
+++ src/home/first-section/description/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Box, Grid } from '@material-ui/core'
+
+import { useStyle } from 'home/first-section/description/style'
+import { useContentStyle } from 'home/first-section/description/hooks/use-content-style'
+import { useWrapperStyle } from 'home/first-section/description/hooks/use-wrapper-style'
+
+const content = `Hermes is a microservice that offers fast deployments of AI characters. 
+Each character can be an independent service with its own dashboard, database and 
+graphql endpoint or it can connect to our global network.`
+
+export const Description = () => <Box classes={useWrapperStyle()}>
+  <Grid item classes={useContentStyle()}>
+    <p className={useStyle().description}>
+      {content}
+    </p>
+  </Grid>
+</Box>
