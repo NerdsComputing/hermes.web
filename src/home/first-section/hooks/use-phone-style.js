bce1313086182bd8228eb891e2b5a4226406c374
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-14 19:50:18 +0300

    [HER-170] Create the phone style of the first section

---
+++ src/home/first-section/hooks/use-phone-style.js
@@ -0,0 +1,8 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  padding: '0 5% 0 5%',
+  backgroundColor: '#F5F7FA',
+  minHeight: '85vh',
+}
+export const usePhoneStyle = makeStyles({ root })
