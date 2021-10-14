74e215bec2ce273f72b2bd569b62cfbfb6243bbe
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-14 19:41:27 +0300

    [HER-170] Create the content from the link section

---
+++ src/home/first-section/link/content.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Grid, Link } from '@material-ui/core'
+
+import { useStyle } from 'home/first-section/link/style'
+import LinkIcon from 'home/first-section/link/link_black_24dp.svg'
+import { useStyles } from 'core/layout/footer/content/style'
+
+export const Content = () => <Grid item>
+  <p className={useStyle().contentFirst}>Git clone </p>
+  <p className={useStyle().contentSecond}>git@github.com:nerds-sh/hermes.cam.git </p>
+  <Link href={'https://github.com/nerds-sh/hermes.web'}>
+    <img src={LinkIcon} alt={'link'} className={useStyles().image} />
+  </Link>
+</Grid>
