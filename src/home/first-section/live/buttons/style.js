11ca5bb546c5bc9952e7ad697093db1da911aa94
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-14 19:39:48 +0300

    [HER-170] Create the style for the arrow svg

---
+++ src/home/first-section/live/buttons/style.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core'
+
+const image = {
+  filter: 'invert(100%) sepia(0%) saturate(0%) hue-rotate(93deg) brightness(103%) contrast(103%)',
+  height: '17px',
+  marginBottom: '-3px',
+}
+
+export const useStyles = makeStyles({ image })
