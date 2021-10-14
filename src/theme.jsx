78529afc179c44dd9d2858ef37c57ebe8e3428a7
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-14 19:56:43 +0300

    [HER-170] Update the theme

--- src/theme.jsx
+++ src/theme.jsx
@@ -2,7 +2,10 @@ import { createMuiTheme } from '@material-ui/core'
 
 export const theme = createMuiTheme({
   palette: {
-    primary: { main: '#52606C' },
+    primary: {
+      main: '#52606C',
+      darker: '#253744',
+    },
     secondary: { main: '#1A856B' },
   },
 })
