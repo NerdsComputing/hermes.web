62b1531fd41665e83c819dcd039b71ec3d498ad6
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-14 19:51:17 +0300

    [HER-170] Create the first section

---
+++ src/home/first-section/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+
+import { Title } from 'home/first-section/title'
+import { Description } from 'home/first-section/description'
+import { Live } from 'home/first-section/live'
+import { Link } from 'home/first-section/link'
+import { useStyle } from 'home/first-section/hooks/use-style'
+
+export const FirstSection = () => <Grid classes={useStyle()}>
+  <Grid container justify={'left'}>
+    <Title />
+    <Description />
+    <Live />
+    <Link />
+  </Grid>
+</Grid>
