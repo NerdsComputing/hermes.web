8c3ff2ef1b8c51d372f79a1a2dcf7ed41a38c61b
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-15 13:10:44 +0300

    [HER-170] Follow-up Add theme as parameter in makeStyles

--- src/home/first-section/description/index.jsx
+++ src/home/first-section/description/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Box, Grid } from '@material-ui/core'
 
-import { useStyle } from 'home/first-section/description/style'
 import { useContentStyle } from 'home/first-section/description/hooks/use-content-style'
 import { useWrapperStyle } from 'home/first-section/description/hooks/use-wrapper-style'
+import { useStyle } from 'home/first-section/description/style'
 
 const content = `Hermes is a microservice that offers fast deployments of AI characters. 
 Each character can be an independent service with its own dashboard, database and 
@@ -11,7 +11,7 @@ graphql endpoint or it can connect to our global network.`
 
 export const Description = () => <Box classes={useWrapperStyle()}>
   <Grid item classes={useContentStyle()}>
-    <p className={useStyle().description}>
+    <p className={useStyle().root}>
       {content}
     </p>
   </Grid>
--- src/home/first-section/description/style.js
+++ src/home/first-section/description/style.js
@@ -1,10 +1,8 @@
 import { makeStyles } from '@material-ui/core'
 
-import { theme } from 'theme'
-
-const description = {
-  fontSize: '16px',
-  color: theme.palette.primary.main,
-}
-
-export const useStyle = makeStyles({ description })
+export const useStyle = makeStyles(theme => ({
+  root: {
+    fontSize: '16px',
+    color: theme.palette.primary.main,
+  },
+}))
