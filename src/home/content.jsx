3dbe2ae25bb2cc7e484d550b8ae0391b5b383cf2
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-15 14:25:59 +0300

    [HER-170] Follow-up Erase inline style from grid

--- src/home/content.jsx
+++ src/home/content.jsx
@@ -4,10 +4,11 @@ import { Grid } from '@material-ui/core'
 import { Marketing } from 'home/marketing'
 import { MeetTeam } from 'home/meet-team'
 import { FirstSection } from 'home/first-section'
+import { useGridStyle } from 'home/first-section/hooks/use-grid-style'
 
 export const Content = () => <Grid container>
   <FirstSection />
-  <Grid item justify={'left'} style={{ padding: '0 15% 0 15%' }}>
+  <Grid item justify={'left'} classes={useGridStyle()}>
     <Marketing />
     <MeetTeam />
   </Grid>
---
+++ src/home/first-section/hooks/use-grid-style.js
@@ -0,0 +1,5 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = { padding: '0 15% 0 15%' }
+
+export const useGridStyle = makeStyles({ root })
