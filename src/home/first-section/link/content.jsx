b606154499914eeb5fddcd2be643c3459975248e
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-15 12:36:41 +0300

    [HER-170] Follow-up Change the git command is lowercase

--- src/home/first-section/link/content.jsx
+++ src/home/first-section/link/content.jsx
@@ -6,7 +6,7 @@ import LinkIcon from 'home/first-section/link/link_black_24dp.svg'
 import { useStyles } from 'core/layout/footer/content/style'
 
 export const Content = () => <Grid item>
-  <p className={useStyle().contentFirst}>Git clone </p>
+  <p className={useStyle().contentFirst}>git clone </p>
   <p className={useStyle().contentSecond}>git@github.com:nerds-sh/hermes.cam.git </p>
   <Link href={'https://github.com/nerds-sh/hermes.web'}>
     <img src={LinkIcon} alt={'link'} className={useStyles().image} />
