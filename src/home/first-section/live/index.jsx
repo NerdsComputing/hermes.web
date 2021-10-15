8132b1484cb9db9664d658d0fcd68099b6d3bbae
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-15 14:27:14 +0300

    [HER-170] Follow-up Add buttons on Live component

--- src/home/first-section/live/index.jsx
+++ src/home/first-section/live/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 
-import { SeeCameras } from 'home/first-section/live/buttons/see-cameras'
-import { SeeDetections } from 'home/first-section/live/buttons/see-detections'
 import { useStyle } from 'home/first-section/live/hooks/use-style'
+import { SeeButton } from 'home/first-section/live/buttons/see-button'
 
 export const Live = () => <Grid item classes={useStyle()} justify={'left'}>
   <Grid container justify={'left'}>
-    <SeeCameras />
-    <SeeDetections />
+    <SeeButton path={'/cameras'} name={'See Cameras'} />
+    <SeeButton path={'/detections'} name={'See Detections'} />
   </Grid>
 </Grid>
