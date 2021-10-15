0d3f51147b36a12ae2dba548b45c240ead5d2693
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-15 17:31:13 +0300

    [HER-170] Follow-up Remove the console.log

--- src/home/first-section/live/see-button/hooks/use-button-style.js
+++ src/home/first-section/live/see-button/hooks/use-button-style.js
@@ -2,7 +2,6 @@ import { useCamerasStyle } from 'home/first-section/live/see-button/hooks/use-ca
 import { useDetectionsStyle } from 'home/first-section/live/see-button/hooks/use-detections-style'
 
 export const useButtonStyle = path => {
-  console.log(path)
   const camerasStyle = useCamerasStyle()
   const detectionsStyle = useDetectionsStyle()
 
