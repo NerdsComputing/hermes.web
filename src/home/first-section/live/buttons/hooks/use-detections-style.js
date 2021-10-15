0eb9538ce8c33624518268e8a1acc6bf19dcab1b
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-15 14:48:02 +0300

    [HER-170] Follow-up Edit the style of the detections button

--- src/home/first-section/live/buttons/hooks/use-detections-style.js
+++ src/home/first-section/live/buttons/hooks/use-detections-style.js
@@ -1,12 +1,11 @@
 import { makeStyles } from '@material-ui/core'
-import { theme } from 'theme'
 
-const root = {
-  marginRight: '24px',
-  textTransform: 'capitalize',
-  background: theme.palette.secondary.main,
-  color: 'white',
-  '&:hover': { background: theme.palette.secondary.main },
-}
+import { buttonStyle } from 'home/first-section/live/buttons/button-style'
 
-export const useDetectionsStyle = makeStyles({ root })
+export const useDetectionsStyle = makeStyles(theme => ({
+  root: {
+    ...buttonStyle,
+    background: theme.palette.secondary.main,
+    '&:hover': { background: theme.palette.secondary.main },
+  },
+}))
