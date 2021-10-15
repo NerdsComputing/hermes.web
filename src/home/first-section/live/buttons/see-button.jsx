43f2ea3cda53b9e224a1357d2b3ded046eeea628
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-15 14:26:38 +0300

    [HER-170] Follow-up Create a single button component

---
+++ src/home/first-section/live/buttons/see-button.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Box, Button, Grid } from '@material-ui/core'
+import { useNavigation } from 'home/first-section/live/hooks/use-navigation'
+import Arrow from 'home/first-section/live/buttons/arrow_right_alt_black_24dp.svg'
+import { useStyles } from 'home/first-section/live/buttons/style'
+import { useButtonStyle } from 'home/first-section/live/buttons/hooks/use-button-style'
+
+export const SeeButton = ({ path, name }) => <Grid item>
+  <Box py={1}>
+    <Button variant={'contained'} onClick={useNavigation(path)} classes={useButtonStyle(path)}>
+      {name}
+      <img src={Arrow} alt={'link'} className={useStyles().image} />
+    </Button>
+  </Box>
+</Grid>
+
+SeeButton.propTypes = {
+  path: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+}
