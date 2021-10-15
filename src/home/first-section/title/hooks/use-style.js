d9c54f3cc2545d1c3c09dcc91e88494201afe4be
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-15 13:39:18 +0300

    [HER-170] Follow-up Change the style for the title

--- src/home/first-section/title/hooks/use-style.js
+++ src/home/first-section/title/hooks/use-style.js
@@ -1,12 +1,14 @@
 import { makeStyles } from '@material-ui/core'
 
-import { theme } from 'theme'
-
 const head = {
   margin: '80px 0 0 0',
   fontSize: '64px',
   fontWeight: 'bold',
-  color: theme.palette.primary.main,
 }
 
-export const useStyle = makeStyles({ head })
+export const useStyle = makeStyles(theme => ({
+  root: {
+    ...head,
+    color: theme.palette.primary.main,
+  },
+}))
--- src/home/first-section/title/index.jsx
+++ src/home/first-section/title/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 
-import { useStyle } from 'home/first-section/title/style'
+import { useStyle } from 'home/first-section/title/hooks/use-style'
 
 export const Title = () => <Grid item>
-  <h1 className={useStyle().head}>Hermes, we build the world</h1>
+  <h1 className={useStyle().root}>Hermes, we build the world</h1>
 </Grid>
