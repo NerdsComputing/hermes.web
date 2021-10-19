e82cf8061c788561aabd385841ce76b8795670d6
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-19 20:36:42 +0300

    [HER-171] Create the style for the subtitle of home page

---
+++ src/home/character-section/hooks/use-subtitle-style.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  fontSize: '38px',
+  fontWeight: 'bold',
+  margin: '144px 0 72px 0',
+}
+
+export const useSubtitleStyle = makeStyles({ root })
