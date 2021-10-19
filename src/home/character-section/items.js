eb94246af971d5e56328434f02adae7775db4e80
Andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-10-19 20:26:26 +0300

    [HER-171] Create characters content

---
+++ src/home/character-section/items.js
@@ -0,0 +1,20 @@
+export const featureCharacters = [
+  {
+    title: 'Air quality - Hippopotamus - Airy',
+    content: `Airy - will monitor air quality during the day and night and it will have a history and data
+        projections available. In the future we will add alerts and triggers on some configurable levels.`,
+    place: 'left',
+  },
+  {
+    title: 'Elephant - Planetina - Water PH sensors',
+    content: `Planetine will monitor and trigger alerts when water PH is threatening it's friend. 
+    Based on PH levels evolution palestine can classify water quality.`,
+    place: 'right',
+  },
+  {
+    title: 'Wolf - Rudy - Forest cutting sensor',
+    content: `Rudy will monitor deforestation activity, it will trigger alerts and provide datasets on the sounds 
+    collected. Rudy's main power will be to alert anyone who registers to that area to threats and illegal activities.`,
+    place: 'left',
+  },
+]
