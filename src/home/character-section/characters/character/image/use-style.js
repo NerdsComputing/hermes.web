5abd011277353cee7b14ee138a570685108ed072
Codrut <constantinescu321@gmail.com>
2021-10-21 14:52:26 +0300

    [HER-171] Follow-up Remove unused styles

--- src/home/character-section/characters/character/image/use-style.js
+++
@@ -1,9 +0,0 @@
-import { useLeftStyle } from 'home/character-section/characters/character/image/left-style'
-import { useRightStyle } from 'home/character-section/characters/character/image/right-style'
-
-export const useStyle = position => {
-  const leftStyle = useLeftStyle()
-  const rightStyle = useRightStyle()
-
-  return position === 'left' ? leftStyle : rightStyle
-}
