ab540cfe5a611c519c1c1727dea23cb50283e23b
Codrut <constantinescu321@gmail.com>
2021-10-21 13:21:46 +0300

    [HER-171] Follow-up Create styles for content

---
+++ src/home/character-section/characters/character/text/content/styles/use-mobile-title-style.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  fontSize: '29px',
+  fontWeight: 'bold',
+  width: '100%',
+}
+
+export const useMobileTitleStyle = makeStyles({ root })
