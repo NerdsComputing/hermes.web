42e67dac2cdbdc1b76dbc444c5bdc816cd75eec3
Codrut <constantinescu321@gmail.com>
2021-10-22 16:49:46 +0300

    [HER-172] Fix component layout

--- src/home/marketing/list/list-link.jsx
+++ src/home/marketing/list/list-link.jsx
@@ -6,7 +6,7 @@ import { useLinkStyle } from 'home/marketing/list/link-style'
 import LinkIcon from 'home/heading-section/link/link_black_24dp.svg'
 
 export const ListLink = ({ link }) => <Link href={link}>
-      <img src={LinkIcon} alt={'link'} className={useLinkStyle().root}/>
-    </Link>
+  <img src={LinkIcon} alt={'link'} className={useLinkStyle().root} />
+</Link>
 
 ListLink.propTypes = { link: PropTypes.string.isRequired }
