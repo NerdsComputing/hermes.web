70c1340765f7c70294e7f3c8ea2542bbe3f1d25b
Codrut <constantinescu321@gmail.com>
2021-10-22 16:47:53 +0300

    [HER-172] Remove log

--- src/home/marketing/list/list-link.jsx
+++ src/home/marketing/list/list-link.jsx
@@ -5,12 +5,8 @@ import PropTypes from 'prop-types'
 import { useLinkStyle } from 'home/marketing/list/link-style'
 import LinkIcon from 'home/heading-section/link/link_black_24dp.svg'
 
-export const ListLink = ({ link }) => {
-  console.log(link)
-
-  return <Link href={link}>
-    <img src={LinkIcon} alt={'link'} className={useLinkStyle().root} />
-  </Link>
-}
+export const ListLink = ({ link }) => <Link href={link}>
+      <img src={LinkIcon} alt={'link'} className={useLinkStyle().root}/>
+    </Link>
 
 ListLink.propTypes = { link: PropTypes.string.isRequired }
