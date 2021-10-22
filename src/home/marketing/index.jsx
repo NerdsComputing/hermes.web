d9de7d1903e847aed16a584203348193f46df0f6
Codrut <constantinescu321@gmail.com>
2021-10-22 15:54:18 +0300

    [HER-172] Create marketing component

--- src/home/marketing/index.jsx
+++ src/home/marketing/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
 
-import { Title } from 'home/marketing/title'
-import { UpperContent } from 'home/marketing/upper-side'
-import { LowerContent } from 'home/marketing/lower-side'
+import { Wrapper } from 'home/marketing/wrapper'
+import { Applications } from 'home/marketing/applications'
+import { Links } from 'home/marketing/links'
+import { Demo } from 'home/marketing/demo'
 
-export const Marketing = () => <Grid item xs={10} s={10} md={10} lg={10}>
-  <Grid container justify={'center'} alignItems={'center'}>
-    <Title />
-    <UpperContent />
-    <LowerContent />
-  </Grid>
-</Grid>
+export const Marketing = () => <Wrapper>
+  <Applications />
+  <Links />
+  <Demo />
+</Wrapper>
