ac3c75b1b1c6931d73372c9166f480181af8d0d2
TBiannca <bia.topircean@gmail.com>
2021-10-25 15:59:41 +0300

    [HER-174] Create image component

---
+++ src/home/first-character/character-section/image.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Box } from '@material-ui/core'
+
+import { useStyles } from 'home/first-character/character-section/use-style'
+
+export const Image = () => <Box classes={useStyles()} />
