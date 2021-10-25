995077f1389712edaa0bb44adb41e4e6efc58b54
TBiannca <bia.topircean@gmail.com>
2021-10-25 16:00:47 +0300

    [her-174] Extract marketing in another component

---
+++ src/home/marketing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Grid} from '@material-ui/core'
+
+import { MeetTeam } from 'home/meet-team'
+
+export const Marketing = () => <Grid item>
+    <Marketing />
+    <MeetTeam />
+</Grid>
