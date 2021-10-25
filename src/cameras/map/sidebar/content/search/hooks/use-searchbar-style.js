bcc7d6e9fc655272ba31d8562cdb66e95c9b232e
Codrut <constantinescu321@gmail.com>
2021-10-25 17:41:48 +0300

    [HER-173] Add new styles

--- src/cameras/map/sidebar/content/search/hooks/use-searchbar-style.js
+++ src/cameras/map/sidebar/content/search/hooks/use-searchbar-style.js
@@ -1,5 +1,9 @@
 import { makeStyles } from '@material-ui/core'
 
-const root = { margin: '5%' }
+const root = {
+  margin: '5% 0 5% 0',
+  border: '1px solid #0000001F',
+  borderRadius: 4,
+}
 
 export const useSearchbarStyle = makeStyles({ root })
