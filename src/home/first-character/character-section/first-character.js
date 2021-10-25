e6f919bf873feace212fd0479189f9c1ff8e2025
TBiannca <bia.topircean@gmail.com>
2021-10-25 15:59:06 +0300

    [her-174] Create firstCharacter object

---
+++ src/home/first-character/character-section/first-character.js
@@ -0,0 +1,7 @@
+export const firstCharacter = {
+  title: 'Owl - Rick -Trash material detector',
+  content: `Morty will monitor and classify any material detected, it will be used to monitor riverbeds, forests, trash
+  bins, for now Morty will only provide full classification data and live streaming of the area. Morty is a tracking 
+  object detection algorithm that can be trained for other detections, like persons. Bikes, animals, very useful as well
+  for smart home automations.`,
+}
