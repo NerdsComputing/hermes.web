b3d1397570886e66996dcc5e073d689ef1e04a56
Codrut <constantinescu321@gmail.com>
2021-10-25 17:36:50 +0300

    [HER-173] Create styles for content

---
+++ src/cameras/map/sidebar/content/styles/desktop-styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  margin: '0 5% 0 5%',
+  width: '90%',
+  opacity: 1,
+}
+
+export const useDesktopStyles = makeStyles({ root })
---
+++ src/cameras/map/sidebar/content/styles/expanded-mobile-styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = {
+  opacity: 1,
+  height: '50%',
+  width: '90%',
+  margin: '0 5% 0 5%',
+}
+
+export const useExpandedMobileStyles = makeStyles({ root })
