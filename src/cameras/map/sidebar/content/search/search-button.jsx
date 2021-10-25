28f9142a038aa561caaf4d33a8a99eaf128e5f1b
Codrut <constantinescu321@gmail.com>
2021-10-25 17:41:25 +0300

    [HER-173] Moved components

--- src/cameras/map/sidebar/content/search/search-button.jsx
+++ src/cameras/map/sidebar/content/search/search-button.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Box, Button, Grid } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
 
-import { useSearchInput } from 'cameras/search/hooks/use-search-input'
+import { useSearchInput } from 'cameras/map/sidebar/content/search/hooks/use-search-input'
 
 export const SearchButton = ({ children }) => <Grid item>
   <Box px={5} py={1} justifyContent={'center'}>
-    <Button variant={'contained'} color={'primary'} onClick={useSearchInput()}>
+    <Button variant={'contained'} color={'secondary'} onClick={useSearchInput()} startIcon={<SearchIcon />}>
       {children}
     </Button>
   </Box>
