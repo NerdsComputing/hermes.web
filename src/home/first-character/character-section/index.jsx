49c534b334a20dd886415d63f6d682d62927d13a
TBiannca <bia.topircean@gmail.com>
2021-10-25 16:01:36 +0300

    [her-174] Create characterSection component

---
+++ src/home/first-character/character-section/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+
+import { Image } from 'home/first-character/character-section/image'
+import { Text } from 'home/first-character/character-section/text'
+
+export const CharacterSection = () => <Grid container>
+  <Image />
+  <Text />
+</Grid>
