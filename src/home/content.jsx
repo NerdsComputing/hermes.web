18de45642c93e0a24c18addd1acd6cfcd8bc7119
TBiannca <bia.topircean@gmail.com>
2021-10-25 15:58:43 +0300

    [HER-174] Add FirstCharacter section

--- src/home/content.jsx
+++ src/home/content.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 
 import { Marketing } from 'home/marketing'
-import { MeetTeam } from 'home/meet-team'
 import { CharacterSection } from 'home/character-section'
 import { HeadingSection } from 'home/heading-section'
+import { FirstCharacter } from 'home/first-character'
 
 export const Content = () => <Grid container>
   <HeadingSection />
+  <FirstCharacter />
   <CharacterSection />
-  <Grid item>
-    <Marketing />
-    <MeetTeam />
-  </Grid>
+  <Marketing />
 </Grid>
