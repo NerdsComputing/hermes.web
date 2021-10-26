5bce7171ea795270e965b53fac58c81d0b578cb9
TBiannca <bia.topircean@gmail.com>
2021-10-26 17:42:33 +0300

    [HER-177] Create style

---
+++ src/home/queries/hooks/use-style.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core'
+
+export const useStyles = makeStyles(() => ({
+  root: { marginLeft: '16px' },
+  align: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}))
