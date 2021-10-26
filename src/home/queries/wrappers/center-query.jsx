e4f6a7948695cca7ec7666677d7c2ebc035cff66
TBiannca <bia.topircean@gmail.com>
2021-10-26 20:08:02 +0300

    [HER-177] Rename hook

--- src/home/queries/wrappers/center-query.jsx
+++ src/home/queries/wrappers/center-query.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 import PropTypes from 'prop-types'
 
-import { useStyles } from 'home/queries/wrappers/hooks/use-style'
+import { useStyle } from 'home/queries/wrappers/hooks/use-styles'
 
-export const CenterQuery = ({ children }) => <Grid container xl={6} lg={6} md={6} sm={12} className={useStyles().align}>
+export const CenterQuery = ({ children }) => <Grid container xl={6} lg={6} md={6} sm={12} className={useStyle().align}>
   { children }
 </Grid>
 
