a43b676f120f316676d5d79c55b8410a0334fcd7
Codrut <constantinescu321@gmail.com>
2021-10-26 16:00:46 +0300

    [HER-173] Remove unused file

--- src/cameras/map/sidebar/header/expand/styles/desktop-styles.js
+++
@@ -1,5 +0,0 @@
-import { makeStyles } from '@material-ui/core'
-
-const root = { opacity: 0 }
-
-export const useDesktopStyles = makeStyles({ root })
--- src/cameras/map/sidebar/header/expand/styles/mobile-styles.js
+++
@@ -1,5 +0,0 @@
-import { makeStyles } from '@material-ui/core'
-
-const root = { opacity: 1 }
-
-export const useMobileStyles = makeStyles({ root })
