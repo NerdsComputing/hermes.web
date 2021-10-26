e30df666420ee6f1cefd2e31649208f67b06dc0e
Codrut <constantinescu321@gmail.com>
2021-10-26 15:24:44 +0300

    [HER-176] Create text component

---
+++ src/cameras/map/marker/info/text/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Grid, Typography } from '@material-ui/core'
+import PropTypes from 'prop-types'
+
+import { useStyles } from 'cameras/map/marker/info/text/style'
+
+export const Text = ({ title, value }) => <Grid item className={useStyles().text}>
+  <Typography varaint={'body1'} color={'primary'} className={useStyles().title}>{`${title}:`}</Typography>
+  <Typography variant={'body1'} color={'primary'} align={'center'} className={useStyles().coordinate}>
+    {`${value}`}
+  </Typography>
+</Grid>
+
+Text.propTypes = {
+  title: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+}
