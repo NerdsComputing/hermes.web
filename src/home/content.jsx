5f43098423a1c34d84221e0866949cbee3c6e40a
TBiannca <bia.topircean@gmail.com>
2021-10-26 17:40:56 +0300

    [HER-177] Add Queries

--- src/home/content.jsx
+++ src/home/content.jsx
@@ -5,9 +5,11 @@ import { MarketingStuff } from 'home/marketing-stuff'
 import { CharacterSection } from 'home/character-section'
 import { HeadingSection } from 'home/heading-section'
 import { FirstCharacter } from 'home/first-character'
+import { Queries } from 'home/queries'
 
 export const Content = () => <Grid container>
   <HeadingSection />
+  <Queries />
   <FirstCharacter />
   <CharacterSection />
   <MarketingStuff />
