7c801918777f551554c1c4ab0d2844ab5ff52a8d
TBiannca <bia.topircean@gmail.com>
2021-10-26 17:43:09 +0300

    [HER-177] Create queries

---
+++ src/home/queries/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Detection } from 'home/queries/detection-query'
+import { Camera } from 'home/queries/camera-query'
+import { CenterQuery } from 'home/queries/center-query'
+
+export const Queries = () => <>
+  <CenterQuery>
+    <Detection />
+  </CenterQuery>
+  <CenterQuery>
+    <Camera />
+  </CenterQuery>
+</>
