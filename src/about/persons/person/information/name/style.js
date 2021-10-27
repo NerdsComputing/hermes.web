f92bd99c63159cb7c0e7d355470f4e26f1115772
Codrut <constantinescu321@gmail.com>
2021-10-27 15:07:13 +0300

    [HER-179] Set width on information name

--- src/about/persons/person/information/name/style.js
+++ src/about/persons/person/information/name/style.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core'
 const root = theme => ({
   fontWeight: 'bold',
   color: theme.palette.white.main,
+  width: '80%',
 })
 
 export const useStyles = makeStyles(theme => ({ root: root(theme) }))
