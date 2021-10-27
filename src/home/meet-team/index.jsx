dab72170792c2676504fa71da275ccb135cc940d
Codrut <constantinescu321@gmail.com>
2021-10-27 13:43:01 +0300

    [HER-179] Remove team meet button

--- src/home/meet-team/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Grid } from '@material-ui/core'
-
-import { NavigationButton } from 'home/meet-team/navigation-button'
-
-export const MeetTeam = () => <Grid container justify={'center'} alignItems={'center'}>
-  <NavigationButton path={'/about'}>Meet our team</NavigationButton>
-</Grid>
