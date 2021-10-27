5d6cda7b2b8228e8cd941819985c7ebdd21f6460
Codrut <constantinescu321@gmail.com>
2021-10-27 13:43:30 +0300

    [HER-179] Create wrapper and styles for characters

--- src/home/first-character/index.jsx
+++ src/home/first-character/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Wrapper } from 'home/character-section/wrapper'
+import { Wrapper } from 'home/first-character/wrapper'
 import { Title } from 'home/first-character/title'
 import { CharacterSection } from 'home/first-character/character-section'
 
---
+++ src/home/first-character/use-phone-style.js
@@ -0,0 +1,5 @@
+import { makeStyles } from '@material-ui/core'
+
+const root = { padding: '0 5% 0 5%' }
+
+export const usePhoneStyle = makeStyles({ root })
